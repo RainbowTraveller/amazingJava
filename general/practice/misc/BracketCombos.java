952849d0c1dc350c66df2f55aa324e544e4b7bb1
Milind Vaidya <MilindArvindVaidya@gmail.com>
2018-08-21 20:38:03 -0700

    Added non recursive implementation

--- general/practice/misc/BracketCombos.java
+++ general/practice/misc/BracketCombos.java
@@ -1,6 +1,15 @@
 import java.util.LinkedList;
 import java.util.LinkedHashSet;
 
+
+
+/*
+ * Print all the valid combinations of a pair of braces
+ * take input at no. of pairs of braces
+ *
+ * LinkedHashSet is used as we need the last set of strings hence order needs to be maintained
+ *
+ */
 public class BracketCombos {
 
     public String prefix(String combo) {
@@ -36,13 +45,45 @@ public class BracketCombos {
         }
     }
 
+
+	public LinkedList<LinkedHashSet<String>> paranthesisComboNR( int count ) {
+
+		LinkedList<LinkedHashSet<String>> combos = new LinkedList<LinkedHashSet<String>>();
+		for(int i = 0; i < count; ++i) {
+			if( i ==0 ) {
+				LinkedHashSet<String> curr = new LinkedHashSet<String>();
+				curr.add( "()" );
+				combos.add(curr);
+			} else {
+				LinkedHashSet<String> curr = new LinkedHashSet<String>();
+				LinkedHashSet<String> prev = combos.getLast();
+				for(String s : prev) {
+					curr.add( prefix( s ) );
+					curr.add( suffix( s ) );
+					curr.add( surround( s  ) );
+				}
+				combos.add( curr );
+			}
+		}
+		return combos;
+	}
+
     public static void main(String[] args) {
         String input = args[0];
         BracketCombos bc = new BracketCombos();
-        LinkedList<LinkedHashSet<String>> al = new LinkedList<LinkedHashSet<String>>();
+        /*LinkedList<LinkedHashSet<String>> al = new LinkedList<LinkedHashSet<String>>();
         bc.paranthesisCombo(al,Integer.parseInt(input));
         LinkedHashSet<String> lhs = al.getLast();
         for(String s : lhs) {
+            System.out.println(s);
+        }*/
+
+		//---------------------------------------------------
+
+		System.out.println(" Non Recursive ");
+        LinkedList<LinkedHashSet<String>> combosNR = bc.paranthesisComboNR( Integer.parseInt( input ) );
+        LinkedHashSet<String> actual = combosNR.getLast();
+        for(String s : actual) {
             System.out.println(s);
         }
     }
