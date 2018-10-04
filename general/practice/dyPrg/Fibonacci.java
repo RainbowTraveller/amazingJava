7bed20e3f0e452397ad5e11ac241ce257c6cb9de
Milind Vaidya <MilindArvindVaidya@gmail.com>
2018-10-03 19:25:44 -0700

    Added 2 more methods to Fibonacci

--- general/practice/dyPrg/Fibonacci.java
+++ general/practice/dyPrg/Fibonacci.java
@@ -1,8 +1,10 @@
 public class Fibonacci {
 	public static void main( String [] args ) {
 		int num = Integer.parseInt( args[0] );
-		long [] mem = new long[ num + 1 ];
+		long [] mem = new long[ num + 2 ];
 		System.out.println( fibboDynamic(num, mem) );
+		System.out.println( fibboDynamicLinear(num, mem) );
+		System.out.println( fibboDynamicSpaceOptimized(num) );
 	}
 
 	//Calculates Fibbo upto 95 without overflowing
@@ -15,5 +17,24 @@ public class Fibonacci {
 		}
 		mem[n] = fibboDynamic( n - 1, mem) + fibboDynamic( n - 2, mem );
 		return mem[ n ];
+	}
+
+	public static long fibboDynamicLinear( int n, long [] fVals) {
+		fVals[ 0 ] = 0;
+		fVals[ 1 ] = 1;
+		for(int i = 2; i <= n; ++i ){
+			fVals[ i ] = fVals[ i - 1 ] + fVals[ i - 2 ];
+		}
+		return fVals[ n ];
+	}
+
+	public static long fibboDynamicSpaceOptimized( int n ) {
+		long sum = 0, a = 0, b = 1;
+		for(int i = 2; i <= n; ++i ){
+			sum = a + b;
+			a = b;
+			b = sum;
+		}
+		return sum;
 	}
 }
