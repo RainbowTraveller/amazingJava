92a7b4854d28c0f4217f3de539d8a57b08277f38
Milind Vaidya <MilindArvindVaidya@gmail.com>
2018-10-16 18:29:42 -0700

    Completed Cumulative and Added LCS 2 more solutions

--- general/practice/misc/CumulativeFrequncyCounter.java
+++ general/practice/misc/CumulativeFrequncyCounter.java
@@ -2,15 +2,7 @@ import java.io.*;
 import java.util.*;
 
 /*
- * To execute Java, please define "static void main" on a class
- * named CumulativeFrequncyCounter.
- *
- * If you need more classes, simply define them inline.
-
-
  Write a function that takes this input as a parameter and returns a data structure containing the number of hits that were recorded on each domain AND each domain under it. For example, an impression on "sports.yahoo.com" counts for "sports.yahoo.com", "yahoo.com", and "com". (Subdomains are added to the left of their parent domain. So "sports" and "sports.yahoo" are not valid domains.)
-
-
 Expected output (in any order):
 1320    com
  900    google.com
@@ -23,85 +15,99 @@ Expected output (in any order):
    3    wikipedia.org
    2    en.wikipedia.org
    1    es.wikipedia.org
-
  */
 
 public class CumulativeFrequncyCounter {
+	public static void main(String[] args) {
+		String[] counts = {
+			"900,google.com",
+			"60,mail.yahoo.com",
+			"10,mobile.sports.yahoo.com",
+			"40,sports.yahoo.com",
+			"300,yahoo.com",
+			"10,stackoverflow.com",
+			"2,en.wikipedia.org",
+			"1,es.wikipedia.org" };
 
-
-  public static void main(String[] args) {
-    String[] counts = {
-      "900,google.com",
-      "60,mail.yahoo.com",
-      "10,mobile.sports.yahoo.com",
-      "40,sports.yahoo.com",
-      "300,yahoo.com",
-      "10,stackoverflow.com",
-      "2,en.wikipedia.org",
-      "1,es.wikipedia.org" };
-
-    List<String> domains = CumulativeFrequncyCounter.getDomains( counts );
-    Map<String, Integer> separatedCounts = new HashMap<String, Integer>();
-
-	System.out.println( domains );
-	separatedCounts = CumulativeFrequncyCounter.getCounts( separatedCounts, domains, counts );
-
-	System.out.println(separatedCounts);
-
-  }
-
-
-  public static List<String> getDomains( String [] input ) {
-    List<String> sources = new LinkedList<String>();
-    for (String source : input ) {
-        sources.add( source.split( ",", 2 )[1]);
-    }
-    return sources;
-  }
-
-
-  public static Map<String, Integer> getCounts(Map<String, Integer> mapping, List<String> domains, String[] originalCounts) {
-	Set<String> uniqueKeys = new HashSet<String>();
-
-    for(String domain : domains) {
-        StringBuffer searchKey = new StringBuffer();
-		//Split the domain name into tokens
-		System.out.println("Domain  : ==> " + domain);
-        String [] splitTokens = domain.split("\\.");
-        int len = splitTokens.length;
-		int startIndex = len - 1;
-		int endIndex = 1;
-
-		if( len <= 2 ) {
-			 endIndex = 0;
-		}
-
-		System.out.println("End index : " + endIndex);
-        for(int i = startIndex; i >= endIndex ; i--) {
-            searchKey.insert(0, splitTokens [ i ]);
-			System.out.println(" ");
-			System.out.println("Token : " + splitTokens[i] );
-            String searchKeyStr = searchKey.toString();
-			if(!uniqueKeys.contains(searchKeyStr)) {
-				for(String oc : originalCounts) {
-				   if( oc.indexOf( searchKeyStr) != -1) {
-					  int count = Integer.valueOf(oc.split(",")[0]);
-					  if(mapping.containsKey( searchKeyStr)) {
-						 int countValue = mapping.get( searchKeyStr );
-						 count += countValue;
-					  }
-					  System.out.println("Search Key : " + searchKeyStr);
-					  mapping.put(searchKeyStr, count);
-				   }
+		//Separate each given domain and the count
+		Map<String, Integer> inputSeparated = separateDomainAndCount( counts );
+		//Process each domain to obtain basic domains to look for
+		Set<String> domains = getDomains( inputSeparated.keySet() );
+		//Search obtained basic domain in the input and check for presence
+		//add the frequency encountered
+		for( String uniqueDomain : domains ) {
+			int count = 0;
+			for(String key : inputSeparated.keySet()) {
+				if( key.contains( uniqueDomain ) ) {
+					count += inputSeparated.get( key );
 				}
 			}
-			searchKey.insert(0, ".");
-			uniqueKeys.add(searchKeyStr);
+			System.out.println( count + "  " + uniqueDomain );
+		}
+	}
+
+
+  /**
+   *  Consider all the input domains and create set of required domains
+   *  call helper function to process each candidate domain string
+   */
+  public static Set<String> getDomains( Set<String> keys ) {
+	Set<String> validDomains = new HashSet<String>();
+    for (String source : keys ) {
+		System.out.println("Current Input domain : " + source);
+		List<String> currentValidDomains = getCurrentValidDomains( source );
+		for( String currentDomain : currentValidDomains ) {
+			//System.out.println("Separated Domain : " + currentDomain);
+			validDomains.add( currentDomain );
 		}
     }
-    return mapping;
+    return validDomains;
   }
 
-  public static Map<String, Integer>
+  /**
+   *  candidate: mail.yahoo.com
+   *  Output :	mail.yahoo.com
+   *			yahoo.com
+   *			com
+   */
+  public static List<String> getCurrentValidDomains( String singleSource ) {
+	  List<String> domains = new ArrayList<String>();
+	  String[] parts = singleSource.split("\\.");
+	  StringBuffer buffer = new StringBuffer();
+	  int pointer = 0;
+	  System.out.println("Parts Found : " + parts.length );
+	  while( pointer < parts.length ) {
+		  for( int i = pointer; i < parts.length; i++ ) {
+			System.out.print(" Part : " + parts[i] + " ");
+			buffer.append( parts[ i ] );
+			buffer.append( "." );
+		  }
+		  buffer.deleteCharAt( buffer.length() - 1);
+		  System.out.println( "Domain Added " + buffer.toString() );
+		  domains.add( buffer.toString() );
+		  buffer.delete( 0, buffer.length());
+		  pointer++;
+	  }
+	  return domains;
+  }
+
+  /**
+   * Separated the frequency from the name of the domain
+   * and stored them as key value pair
+   * where domain is the key and the value is the frequency
+   */
+  public static Map<String, Integer> separateDomainAndCount( String[] input ) {
+	  Map<String, Integer> inputDomainToCount = new HashMap<String, Integer>();
+	  for(String i : input ) {
+		  String[] inputSplit = i.split(",");
+		  if(inputSplit.length == 2) {
+			  String domain = inputSplit[ 1 ];
+			  int count = Integer.valueOf( inputSplit[ 0 ] );
+			  System.out.println("Domain : " + domain + " -- " + "Count : " + count);
+			  inputDomainToCount.put( domain, count );
+		  }
+	  }
+	  return inputDomainToCount;
+  }
 }
 
--- general/practice/strings/LongestCommonSubsequence.java
+++ general/practice/strings/LongestCommonSubsequence.java
@@ -5,9 +5,67 @@
 */
 public class LongestCommonSubsequence {
 
+	public static int mem[][];
 	public static void main( String[] args ) {
 		System.out.println( LCS(  "abcdaf", "acbcf" ));
+		System.out.println( LCSRecursive(  "abcdaf", "acbcf" ));
+		System.out.println( LCSRecursiveMem(  "abcdaf", "acbcf" ));
 	}
+
+	public static int LCSRecursiveMem( String s1, String s2 ) {
+		if( s1 != null && s2 != null ) {
+			int len1 = s1.length();
+			int len2 = s2.length();
+			mem = new int[len1][ len2];
+			for(int i = 0; i < len1; ++i) {
+				for(int j = 0; j < len2; ++j) {
+					mem[i][j] = -1;
+				}
+			}
+			return LCSRecursiveMemHelper(0, 0, len1, len2, s1, s2);
+		}
+		return -1;
+	}
+
+	private static int LCSRecursiveMemHelper( int i, int j, int len1, int len2, String s1, String s2) {
+		if( i >= len1 || j >= len2 ) {
+			return 0;
+		}
+
+		if( mem[i][j] < 0 ) {
+			if( s1.charAt( i  ) == s2.charAt( j  ) ) {
+				mem[i][j] = 1 + LCSRecursiveMemHelper( i + 1, j + 1, len1, len2, s1, s2);
+			} else {
+				mem[i][j] =  Math.max( LCSRecursiveMemHelper( i, j + 1, len1, len2, s1, s2),
+					LCSRecursiveMemHelper( i + 1, j, len1, len2, s1, s2));
+			}
+		}
+		return mem[i][j];
+	}
+
+	public static int LCSRecursive( String s1, String s2 ) {
+		if( s1 != null && s2 != null ) {
+			int len1 = s1.length();
+			int len2 = s2.length();
+			return LCSRecursiveHelper(0, 0, len1, len2, s1, s2);
+		}
+		return -1;
+	}
+
+	private static int LCSRecursiveHelper( int i, int j, int len1, int len2, String s1, String s2 ) {
+		if( i >= len1 || j >= len2 ) {
+			return 0;
+		}
+
+		if( s1.charAt( i  ) == s2.charAt( j  ) ) {
+			return 1 + LCSRecursiveHelper( i + 1, j + 1, len1, len2, s1, s2 );
+		}
+
+		return Math.max( LCSRecursiveHelper( i, j + 1, len1, len2, s1, s2 ),
+			LCSRecursiveHelper( i + 1, j, len1, len2, s1, s2 )
+		);
+	}
+
 	public static int LCS( String a, String b) {
 		if( a != null && b != null) {
 			int max = 0;
