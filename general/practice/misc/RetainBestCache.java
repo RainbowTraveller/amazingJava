93e449fa26270d335c341df5be4e3de4b4c5b318
Milind Vaidya <MilindArvindVaidya@gmail.com>
2018-10-22 20:27:12 -0700

    Solved Locker problem

--- general/practice/misc/RetainBestCache.java
+++ general/practice/misc/RetainBestCache.java
@@ -87,4 +87,3 @@ interface DataSource<K, T extends Rankable> {
 
 
 
-/*It is the end of the school year, and the graduating class of Moore Science High (n students in all) has decided to end the year by making quite a lot of noise. They line up in the locker room, where all n lockers are closed. The first student runs down the row of lockers, opening each one. Then, the second runs down, slamming every other locker (this is where the noise comes in). The third student then runs down, opening (or slamming) every third locker, and so on until all n students have gone through, each opening (or closing) each locker evenly divisible by i, where i is the ith student. Write a function that prints each locker that is open after the students are done.*/
