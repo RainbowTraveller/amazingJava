52f9003c08e3fe25bb4155901471e35e65cb2bd1
Milind Vaidya <MilindArvindVaidya@gmail.com>
2018-11-11 13:16:23 -0800

    Added = to correct the logic

--- general/practice/sorting/QuickSort.java
+++ general/practice/sorting/QuickSort.java
@@ -4,16 +4,16 @@ public class QuickSort {
 
     public static void quickSort (int [] arr, int low, int high) {
         if(low < high) {
-			int pivot = arr[ (low + high) / 2 ];
-            int index = partition(arr, low, high, pivot);
-            System.out.println("PIVOT INDEX: " + index);
-            quickSort(arr, low, index - 1);
-            quickSort(arr, index, high);
+			int chosenPivot =  arr[(low + high) / 2] ;
+			int index = partition(arr, low, high, chosenPivot);
+			System.out.println("PIVOT INDEX: " + index);
+			quickSort(arr, low, index - 1);
+			quickSort(arr, index, high);
         }
     }
 
     public static int partition(int [] arr, int low, int high, int pivot) {
-		while( low < high ) {
+		while( low <= high ) {
 			while( arr[low] < pivot ) {
 				low++;
 			}
@@ -35,6 +35,7 @@ public class QuickSort {
     public static void main(String [] args) {
         int [] input = {2, 4, 7, 1, 5, 8, 6, 34, 563, 1973, 12, 3, 45, 67, 91, 10};
         //int [] input = {2, 4, 7, 1, 3, 5, 8, 6};
+        //int [] input = { 10, 7, 5, 1, 3, 4, 6 ,2, 9, 8 };
         quickSort(input, 0, input.length - 1);
         System.out.println(Arrays.toString(input));
     }
