5f809cbaebdced1d894c8ba1e5eed4ea12642058
Milind Vaidya <MilindArvindVaidya@gmail.com>
2018-12-01 16:19:48 -0800

    Added method using map

--- general/practice/arrays/NosEqualToSum.java
+++ general/practice/arrays/NosEqualToSum.java
@@ -1,24 +1,46 @@
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
 
 public class NosEqualToSum {
-
     public static void main(String[] args) {
         int[] arr = {2, 10, 0, 25, 17, 20, 41, 23, 30, 5, 13, 39, 7};
         int sum = 30;
-        Arrays.sort(arr);
-        int start = 0;
-        int end = arr.length - 1;
+		//Get Indexes of the elements
+		getPairIndexes( arr, sum );
+		System.out.println("Another method :");
+		//Get Values of the elements
+		getPairValues( arr, sum );
+	}
 
-        while(start < end) {
-            if(arr[start] + arr[end] == sum) {
-                System.out.println("PAIR: " + arr[start] + " " + arr[end]);
-                start++;
-                end--;
-            } else if(arr[start] + arr[end] < sum) {
-                start++;
+	public static void getPairValues(int [] arr, int sum ) {
+		Arrays.sort(arr);
+		int start = 0;
+		int end = arr.length - 1;
+
+		while(start < end) {
+			if(arr[start] + arr[end] == sum) {
+				System.out.println("PAIR: " + arr[start] + " " + arr[end]);
+				start++;
+				end--;
+			} else if(arr[start] + arr[end] < sum) {
+				start++;
+			} else {
+				end--;
+			}
+		}
+	}
+
+	public static void getPairIndexes( int [] nums, int target) {
+		Map<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; ++i) {
+            if(tracker.containsKey(nums[i])) {
+				System.out.println("Pair : " + tracker.get(nums[i])  + " " + i);
             } else {
-                end--;
+				//Store the diff value and index of current value, so that if
+				//corr pair value is encountered we have
+                tracker.put(target - nums[i], i);
             }
         }
-    }
+	}
 }
