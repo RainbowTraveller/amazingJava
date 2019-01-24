05f9af762c0a10e5d7f948aa4eb0ba535839bc23
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-01-23 19:45:11 -0800

    Tested, sops and comments

--- general/practice/strings/WordLadder.java
+++ general/practice/strings/WordLadder.java
@@ -2,39 +2,46 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.*;
 /*
-stone
-Atone
-aLone
-Clone
-clonS
-cOons
-coNns
-conEs
-coneY
-Money
-   Word ladders were invented by Lewis Carroll in 1878, the author of Alice in Wonderland.
-   A ladder is a sequence of words that starts at the starting word, ends at the ending word, In a word ladder puzzle you have to change one word into another by altering a single letter at each step.
-   Each word in the ladder must be a valid English word, and must have the same length.
-   For example, to turn stone into money, one possible ladder is given on the left.
-Many ladder puzzles have more than one possible solutions. Your program must determine a shortest word ladder. Another path from stone to money is
+    stone
+    Atone
+    aLone
+    Clone
+    clonS
+    cOons
+    coNns
+    conEs
+    coneY
+    Money
 
-stone  store  shore  chore  choke  choky  cooky  cooey  coney  money
+   Word ladders were invented by Lewis Carroll in 1878, the author of Alice in Wonderland. A ladder is a sequence of words that starts at the starting word, ends at the ending word,
+   In a word ladder puzzle you have to change one word into another by altering a single letter at each step. Each word in the ladder must be a valid English word, and must have the
+   same length. For example, to turn stone into money, one possible ladder is given on the left. Many ladder puzzles have more than one possible solutions. Your program must determine
+   a shortest word ladder. Another path from stone to money is
+
+   stone  store  shore  chore  choke  choky  cooky  cooey  coney  money
+
+   https://www.cs.cmu.edu/~adamchik/15-121/labs/HW-4%20Word%20Ladder/lab.html : explaination and further testing
+
  */
 public class WordLadder {
-    private Set<String>             dictionary; // dictionary
-    private Deque<LinkedList<String>>    wordLadder;
-    private String                  start;
-    private String                  end;
-
+    // dictionary contains available words
+    private Set<String>                 dictionary;
+    //Queue of stack of word ladders. Each stack contains a ladder from starting
+    //word to an intermediate word in the dictionary
+    private Deque<LinkedList<String>>   wordLadder;
+    private String                      start;
+    private String                      end;
 
     public static void main(String [] args) {
+        //Get start and end words
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first word");
         String start = sc.nextLine();
         System.out.println("Enter the last word");
         String end = sc.nextLine();
         WordLadder wl = new WordLadder( start, end );
-        wl.getInput(sc);
+        //Get the words in the dictionary
+        //wl.getInput(sc);
         wl.findShortestLadder();
     }
 
@@ -42,12 +49,26 @@ public class WordLadder {
         this.start = start;
         this.end = end;
         dictionary = new LinkedHashSet<String>();
+        dictionary.add( "conk");
+        dictionary.add( "blue");
+        dictionary.add( "cork");
+        dictionary.add( "blae");
+        dictionary.add( "pork");
+        dictionary.add( "perk");
+        dictionary.add( "blad");
+        dictionary.add( "perl");
+        dictionary.add( "bead");
         wordLadder = new LinkedList<LinkedList<String>>();
+        //Create initial stack with only start word
+        //and add that stack to the queue
         LinkedList<String> startList = new LinkedList<String>();
         startList.addFirst( this.start );
         wordLadder.add(startList);
     }
 
+    /*
+     * Accepts the words in the dictionary for reference
+     */
     public void getInput(Scanner sc) {
         System.out.println("Please enter the no. of words in the dictionary: ");
         int numwords = sc.nextInt();
@@ -58,6 +79,9 @@ public class WordLadder {
         }
     }
 
+    /*
+     * Checks if 2 words differ only by 1 character
+     */
     private boolean isNextWord(String current, String next) {
         int len = current.length();
         int count = 0;
@@ -70,7 +94,6 @@ public class WordLadder {
     }
 
 
-
     public void setDictionary( Set<String> modifiedDictionary ){
         this.dictionary = modifiedDictionary;
     }
@@ -80,8 +103,10 @@ public class WordLadder {
         while(!wordLadder.isEmpty()) {
             //Remove first stack of string from the word ladder queue
             LinkedList<String> currentStackOfStrings = wordLadder.pollFirst();
+            System.out.println("Current Stack : " + currentStackOfStrings);
             //Get the string at the stack top
-            String candidate = currentStackOfStrings.getFirst();
+            String candidate = currentStackOfStrings.getLast();
+            System.out.println("Candidate : " + candidate);
             if(!dictionary.isEmpty() && !this.end.equals(candidate)) {
                 Set<String> newDictionary = new LinkedHashSet<String>();
                 for(String word : dictionary) {
@@ -96,9 +121,11 @@ public class WordLadder {
                 }
                 dictionary = newDictionary;
             }
+            length = currentStackOfStrings.size();
             if(this.end.equals(candidate)) {
                 while(!wordLadder.isEmpty()) {
                     currentStackOfStrings = wordLadder.pollFirst();
+                    System.out.println("Current Stack 2 : " + currentStackOfStrings);
                     if(currentStackOfStrings.size() < length) {
                         length = currentStackOfStrings.size();
                     }
