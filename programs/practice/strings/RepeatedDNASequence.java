faaa578764dc6fd68e53fa43c99495ee035eed4d
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-02-15 19:02:42 -0800

    Finding repeated subsequences

---
+++ programs/practice/strings/RepeatedDNASequence.java
@@ -0,0 +1,46 @@
+/*
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+Example:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+
+Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+
+*/
+import java.util.*;
+public class RepeatedDNASequence {
+
+
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        List<String> answer = findRepeatedDnaSequences(input);
+        for(String s : answer) {
+            System.out.println(s);
+        }
+    }
+
+    public static List<String> findRepeatedDnaSequences(String s) {
+        List<String> seq = new LinkedList<String>();
+        Map<String, Integer> tracker = new HashMap<String, Integer>();
+        if(s != null && s.length() >= 10) {
+
+            for(int i = 0; i + 10 <= s.length(); ++i) {
+                String current = s.substring(i, i + 10);
+                //System.out.println(current);
+                int frequency = tracker.getOrDefault(current, 0);
+                tracker.put(current, frequency + 1);
+            }
+        }
+        for(String str : tracker.keySet()) {
+            int count = tracker.get(str);
+            if(count > 1) {
+                seq.add(str);
+            }
+        }
+        return seq;
+    }
+}
