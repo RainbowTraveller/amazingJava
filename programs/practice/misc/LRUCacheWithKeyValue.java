a7471f59008ee612905cbeaa0a7e3af1aeae1515
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-03-16 16:44:17 -0700

    Need to adjust diff keys and same value scenario

--- programs/practice/misc/LRUCacheWithKeyValue.java
+++ programs/practice/misc/LRUCacheWithKeyValue.java
@@ -1,5 +1,4 @@
-class LRUCache {
-
+class LRUCacheWithKeyValue {
     LinkedList<Integer> cache;
     Map<Integer, Integer> tracker;
     int capacity;
@@ -11,56 +10,54 @@ class LRUCache {
     }
 
     public int get(int key) {
-
+        int val = -1;
         if( tracker.containsKey(key) ) {
             int indexIntoList = tracker.get(key);
-            //System.out.println(key + " " + indexIntoList);
-            adjust(key);
+            System.out.println("Get");
             System.out.println("Cache : " + cache);
-            int val = cache.remove(indexIntoList);
-            put(key, val);
-            return val;
+            System.out.println("Tracker" + tracker);
+            val = cache.get(indexIntoList);
         }
-        return -1;
-
+        put(key, val);
+        return val;
     }
 
     public void put(int key, int value) {
-		if(tracker.containsKey(key)) {
-			get(key);
-		}
-
-        if(cache.size() == capacity) {
-            adjust( -1 );
+        if(tracker.containsKey(key)) {
+            int index = tracker.get(key);
+            adjust(index);
+            cache.remove(index);
+            for(int currKey : tracker.keySet()) {
+                if(tracker.get(currKey) == index) {
+                    tracker.remove(currKey);
+                    break;
+                }
+            }
+        } else if(cache.size() == capacity) {
+            for(int currKey : tracker.keySet()) {
+                if(tracker.get(currKey) == 0) {
+                    tracker.remove(currKey);
+                    break;
+                }
+            }
+            adjust(0);
             cache.removeFirst();
         }
         cache.addLast(value);
         tracker.put(key, cache.size() - 1);
-        System.out.println(cache);
-        //System.out.println(tracker);
+        System.out.println("Put");
+        System.out.println("Cache : " + cache);
+        System.out.println("Tracker" + tracker);
     }
 
-    private void adjust( int key ) {
-        int indexIntoList = 0;
-        if( key != -1) {
-            indexIntoList = tracker.get( key );
-            tracker.remove( key );
-        }
-
-        int specialKey = -1;
+    private void adjust( int index ) {
         for(int currKey : tracker.keySet()) {
             int currIndex = tracker.get(currKey);
-            if(key == -1 && currIndex == 0) {
-                specialKey = currKey;
-            } else if( currIndex > indexIntoList) {
+            if( currIndex > index) {
                 currIndex--;
                 tracker.put(currKey, currIndex);
             }
         }
-        if(specialKey > 0) {
-            tracker.remove(specialKey);
-        }
-
     }
 }
 
@@ -70,3 +67,4 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
