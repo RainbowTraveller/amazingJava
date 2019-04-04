d4b0dc0d1d4ecdb733e33dbf3deadc9cd1818f1f
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-04-03 19:43:09 -0700

    Added simpler and efficient version

--- programs/practice/misc/KEqualSumSeparation.java
+++ programs/practice/misc/KEqualSumSeparation.java
@@ -3,173 +3,79 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class KEqualSumSeparation {
-	/**
-	*
-	*/
-	private boolean separationHelper(List<Integer> input, int perSetSum, int[] subsetSumTracker, boolean[] used, int index, int len, int noOfPartitions) {
-		if(subsetSumTracker[index] == perSetSum) {
-			if(index == noOfPartitions - 2) {
-				//We already checked if the k partitions are possible if index is (k - 2) which means (k - 1)
-				//are possible, so will be kth, no need to check for that explicitly
-				return true;
-			}
-			//check for more partions
-			return separationHelper(input, perSetSum, subsetSumTracker, used, index + 1, len, noOfPartitions);
-		}
-		//check for desired sum in the list
-		for(int i = 0; i < len; ++i) {
-			if(!used[i]) {
-				int currNum = input.get(i);
-				int sum = subsetSumTracker[index];
-				if(sum < perSetSum) {
-					used[i] = true;
-					subsetSumTracker[index] += currNum;
-                    if( separationHelper(input, perSetSum, subsetSumTracker, used, index, len, noOfPartitions) ) {
-                        System.out.println("CURRNUM " + currNum);
-                        return true;
-                    } else {
-						used[i] = false;
-						subsetSumTracker[index] -= currNum;
-					}
-				}
-			}
-		}
-		return false;
-	}
-
-	public boolean separate(List<Integer> list, int k) {
-		if(list == null) {
-			return false;
-		}
-		if(k == 1) {
-			return true;
-		}
-
-		int len = list.size();
-		if(len < k) {
-			return false;
-		}
-
-        Collections.sort(list);
-		int sum = 0;
-
-		for(int num : list) {
-			sum += num;
-		}
-		if(sum % k != 0) {
-			return false;
-		}
-
-		int perSetSum = sum / k;
-        //track the sum
-		int[] subsetSumTracker = new int [k];
-        //track used elements
-		boolean[] used =  new boolean[len];
-
-        //Initialise
-		for (int i = 0; i < k; i++)
-			subsetSumTracker[i] = 0;
-		for (int i = 0; i < len; i++)
-			used[i] = false;
-
-		return separationHelper(list, perSetSum, subsetSumTracker, used, 0, len, k);
-	}
-
-    public boolean canPartitionKSubsets(List<Integer> list, int k) {
-
-        if(list != null && list.size() > k ) {//size should be > than K and not null
-            if(k == 1) {// always possible to divide in 1 group
-                return true;
-            }
-            Collections.sort(list);//Sorting for reducing compute time
-            int sum = 0;
-            for(int num : list) {
-                sum += num;
-            }
-
-            //Trying to reduce cyclomatic complexity ( independent paths in the program like return true or false every now and then )
-            if(sum % k == 0) { // sum should be evenly distributed into k parts
-                int targetSum = sum / k;
-                int targetSize = list.size() - 1;
-                if(list.get(targetSize) <= targetSum) {//if greatest element after sorting is > targetSum, division is not possible
-                    while(targetSize > 0 && list.get(targetSize) == targetSum) {//reduce the work by no considering elements equal to targetSum
-                        targetSize--;//reduce elements to consider
-                        k--;// and of course less no of partitions accordingly
-                    }
-                }
-                //Function to be called recursively
-                //K partitions required
-                //list : original list
-                //targetSize : where to start to consider elements from list, greatest less than targetSum
-                //targetSum  : actual sum to reach
-                return search( new int[k], list, targetSize, targetSum );
-            }
-        }
-        return false;
-    }
-
-    //Starting from greatest no. less than target Sum and going down considering other lesser
-    //nos. one by one. So slots are filled by nos if it is zero and search continues to see
-    //if any no. added satisfies the targetSum criteria. if not the no. if removed from slot sum
-    //and next lesser nos. are tried
-    public boolean search(int[] sumGroups, List<Integer> list, int targetSize, int targetSum) {
-        if(targetSize < 0) {
-            //This is not reached unless everything is fine and equals sums have achieved
-            return true;
-        }
-
-        int currentNum = list.get(targetSize--);
-        for(int i = 0; i < sumGroups.length; ++i) {
-            if(sumGroups[i] + currentNum <= targetSum) {
-                sumGroups[i] += currentNum;
-                System.out.println("SUM  " + sumGroups[i]);
-                if(search(sumGroups, list, targetSize, targetSum)) {
-                    return true;
-                }
-                //Not working with current no. added to this slot for meet
-                //target sum, so let's subtract it from the current slot
-                //and check if any subsequent no. satisfies our criteria
-                sumGroups[i] -= currentNum;
-            }
-            //Current no. was the only once remaining in this slot
-            //No other matching no. was found for this no. which means
-            //result is false. No need to check for other slots etc.
-            if(sumGroups[i] == 0) {
-                break;
-            }
-        }
-        //Current number could not fit into any slot available
-        //meaning no sum equal to target sum is possible for this no.
-        //hence overall result is false
-        return false;
-    }
-
 	public static void main(String[] args) {
-		List<Integer> input = new ArrayList<Integer>();
         //False
         //[2,2,2,2,3,4,5]
         //4
         //True
         //[129,17,74,57,1421,99,92,285,1276,218,1588,215,369,117,153,22]
         //3
-		input.add(129);
-		input.add(17);
-		input.add(74);
-		input.add(57);
-		input.add(1421);
-		input.add(99);
-		input.add(92);
-		input.add(285);
-		input.add(1276);
-		input.add(218);
-		input.add(1588);
-		input.add(215);
-		input.add(369);
-		input.add(117);
-		input.add(153);
-		input.add(22);
+        int [] arr = {129,17,74,57,1421,99,92,285,1276,218,1588,215,369,117,153,22};
 		KEqualSumSeparation findkseparations = new KEqualSumSeparation();
-		//System.out.println(findkseparations.separate(input, 3));
-		System.out.println(findkseparations.canPartitionKSubsets(input, 3));
+        System.out.println(findkseparations.canPartitionKSubsets(arr, 3));
 	}
+
+    public boolean canPartitionKSubsets(int[] arr, int k) {
+        // The total sum to be considered
+        int sumOfAllArrayItems = 0;
+        for (int num : arr) {
+            sumOfAllArrayItems += num;
+        }
+
+        /*
+        * 1. We can not have negative partitions
+        * 2. We can not divide x no of elements into y parts where y > x
+        *    e.g. divide 10 things into 100 groups ( as a whole and not part of a object should be considered )
+        * 3. If sum can not be distributed equally into k parts, no need to proceed
+        */
+        if (k <= 0 || k > arr.length || sumOfAllArrayItems % k != 0) {
+            return false;
+        }
+
+        return canPartition(0, arr, new boolean[arr.length], k, 0, sumOfAllArrayItems / k);
+    }
+
+    boolean canPartition(int iterationStart, int[] arr, boolean[] used, int k,
+                          int inProgressBucketSum, int targetBucketSum) {
+        /*
+         * This is top down approach. We start with k required partitions and when we get one
+         * we reduce the k by 1.
+         *
+         * Now k = 1, which means that we have found k - 1 partitions already. which contain target sum
+         * and we already made sure that the sum is evenly distributed into k parts. So if k - 1 partitions are
+         * obtained, no need to process for last partitions, previously checked conditions indicate so.We have
+         * made sure that K partitions are indeed possible
+         *
+         */
+        if( k == 1 ) {
+            return true;
+        }
+
+        //Check if for this partition, criteria is met
+        //if yes then reduce the required target partitions, with targetSum remaining same
+        if (inProgressBucketSum == targetBucketSum) {
+            return canPartition(0, arr, used, k - 1, 0, targetBucketSum);
+        }
+        /*
+         * Crux of the solution:
+         * essentially start from 0 and check which elements come together to add up and form desired sum
+         * now when an element is considered, call recursively and start from that index onwards.
+         * Also when an element is used mark it used.
+         * But what if it fails to add up...that means the element you started with was not appropriate one
+         * so be it ...don't consider it and mark it not used and try other elements
+         * If satisfied though, 1 less bucket to consider out of k buckets
+         */
+        for (int i = iterationStart; i < arr.length; i++) {
+            if (!used[i] && inProgressBucketSum + arr[i] <= targetBucketSum) {
+                //Mark it used so nobody else in current iteration uses it
+                used[i] = true;
+                if (canPartition(i + 1, arr, used, k, inProgressBucketSum + arr[i], targetBucketSum)) {
+                    return true;
+                }
+                //this did not work, ok mark it not used
+                used[i] = false;
+            }
+        }
+        return false;
+    }
 }
