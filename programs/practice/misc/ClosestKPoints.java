138620a2e25d2e43fb817fb2d1b2de6f861e91c9
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-05-25 17:52:23 -0700

    Added a simpler approach

--- programs/practice/misc/ClosestKPoints.java
+++ programs/practice/misc/ClosestKPoints.java
@@ -1,5 +1,29 @@
+/*
+ *
+ We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation:
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+
+Example 2:
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+
+*/
 import java.util.PriorityQueue;
 import java.util.Comparator;
+import java.util.Arrays;
 
 class ClosestKPoints {
 
@@ -27,6 +51,10 @@ class ClosestKPoints {
         for(int i = 0; i < desired.length; ++i) {
           System.out.println("Point " + (i + 1) + " X : " + desired[i][0] + " Y : " + desired[i][1] );
         }
+        desired = kClosestSimple(points, 7);
+        for(int i = 0; i < desired.length; ++i) {
+          System.out.println("Point " + (i + 1) + " X : " + desired[i][0] + " Y : " + desired[i][1] );
+        }
     }
 
     public static int[][] kClosest(int[][] points, int K) {
@@ -36,14 +64,14 @@ class ClosestKPoints {
 
         Comparator<Point> compareDistance = new Comparator<Point>() {
             public int compare(Point p1, Point p2) {
-								 if(p1.distance() == p2.distance()) {
+                 if(p1.distance() == p2.distance()) {
                     return 0;
                 } else if ( p1.distance() > p2.distance()) {
                     return 1;
                 } else {
                     return -1;
                 }
-								//This will not give perfect answer as the distance can be long
+                //This will not give perfect answer as the distance can be long
                 //return p1.distance() - p2.distance();
             }
         };
@@ -65,4 +93,32 @@ class ClosestKPoints {
         }
         return closest;
     }
+
+    public static int[][] kClosestSimple(int[][] points, int K) {
+        int N = points.length;
+        int[] dists = new int[N];
+
+        //Calculate distances
+        for (int i = 0; i < N; ++i)
+            dists[i] = dist(points[i]);
+
+        //Sort
+        Arrays.sort(dists);
+        //Get desired largest distance
+        int distK = dists[K-1];
+
+        int[][] ans = new int[K][2];
+        int t = 0;
+        //Now gather only points that are less than or equal to
+        //distK
+        for (int i = 0; i < N; ++i)
+            if (dist(points[i]) <= distK)
+                ans[t++] = points[i];
+        return ans;
+    }
+
+    public static int dist(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
 }
+
