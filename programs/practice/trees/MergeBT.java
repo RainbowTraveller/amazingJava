aea188ddb94ede9356a9a1f0b3784f64a15c6e92
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-06-03 20:20:58 -0700

    Added comments and fixed indentation

--- programs/practice/trees/MergeBT.java
+++ programs/practice/trees/MergeBT.java
@@ -1,32 +1,37 @@
 /*
  *
  * Add the values of two nodes at the same level
+ * The two trees may not be identical to each other meaning either of the
+ * tree may not have corresponding node for the other tree
  *
  */
 public class MergeBT {
-	//Definition for a binary tree node.
-	public class TreeNode {
-	  int val;
-	  TreeNode left;
-	  TreeNode right;
-	  TreeNode(int x) { val = x; }
-	}
-	public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-		if(t1 == null && t2 == null) {
-			return null;
-		} else {
-			if(t1 != null && t2 == null) {
-				return t1;
-			} else if ( t2 != null && t1 == null) {
-				return t2;
+    //Definition for a binary tree node.
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        //If both are null nothing to do
+        if(t1 == null && t2 == null) {
+            return null;
+        } else {
+            //Take whichever is not null
+            if(t1 != null && t2 == null) {
+                return t1;
+            } else if ( t2 != null && t1 == null) {
+                return t2;
 
-			} else {
-				t1.val += t2.val;
-				t1.left = mergeTrees(t1.left, t2.left);
-				t1.right = mergeTrees(t1.right, t2.right);
-				return t1;
-			}
-		}
+            } else {
+                //both are not null so process
+                t1.val += t2.val;
+                t1.left = mergeTrees(t1.left, t2.left);
+                t1.right = mergeTrees(t1.right, t2.right);
+                return t1;
+            }
+        }
 
-	}
+    }
 }
