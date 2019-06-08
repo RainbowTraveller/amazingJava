5daee03099fde83c6236efebbfe869e51bd63c26
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-06-07 18:51:33 -0700

    Added recursive and momeoisation methods

--- programs/practice/misc/StepsCalculation.java
+++ programs/practice/misc/StepsCalculation.java
@@ -16,31 +16,62 @@ public class StepsCalculation {
  * So it goes without saying that the total number of solution to reach the point [n] should be [n-1] + [n-2].
  */
     public static int steps(int step) {
-		if(step <=0) {
-			return 0;
-		}
-		if(step == 1) {
-			return 1;
-		}
-		if(step == 2) {
-			return 2;
-		}
+        if(step <=0) {
+            return 0;
+        }
+        //Base Case for staircase with 1 step
+        if(step == 1) {
+            return 1;
+        }
+        //Base Case for staircase with 2 steps
+        if(step == 2) {
+            return 2;
+        }
 
-		int one_step_before = 1;
-		int two_step_before = 2;
-		int all_steps = 0;
-		for(int i = 2; i < step; ++i) {
-			 all_steps = one_step_before + two_step_before;
-			 one_step_before = two_step_before;
-			 two_step_before = all_steps;
-		}
-		return all_steps;
+        //Steps until 1 steps before current step
+        int one_step_before = 2;
+        //Steps until 2 steps before current step
+        int two_step_before = 1;
+        int all_steps = 0;
+        //We start from 3 (for base 0, loop starts from 2)
+        //so above initialization if for step no. 3
+        for(int i = 2; i < step; ++i) {
+            all_steps = one_step_before + two_step_before;
+            two_step_before = one_step_before;
+            one_step_before  = all_steps;
+        }
+        return all_steps;
+    }
+
+    /*
+     * This is easiest to code as we are solving subproblems recursively
+     * but some of the subproblems are solved repeatedly, hence we have exponential
+     * complexity. We can reduce this with memoisation
+     */
+    public static int stepsRecursive(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        return stepsRecursive( n - 1 ) + stepsRecursive( n - 2 );
+    }
+
+    public static int stepsMemo(int i, int [] steps) {
+        if(steps[ i - 1 ] == -1) {
+            steps[ i - 1 ] = stepsMemo( i - 1, steps) + stepsMemo( i - 2, steps);
+        }
+        return steps[i - 1];
     }
 
     public static void main(String[] args) {
-		int i = Integer.valueOf(args[0]);
-        System.out.println(steps(i));
-
+        int i = Integer.valueOf(args[0]);
+        System.out.println("Iterative : " + steps(i));
+        System.out.println("Recursive : " + stepsRecursive(i));
+        int[] stepsTilln = new int [i];
+        for(int j = 0; j< i ; ++j) {
+            stepsTilln[j] = -1;
+        }
+        stepsTilln[0] = 1;
+        stepsTilln[1] = 2;
+        System.out.println("Memo : " + stepsMemo(i, stepsTilln));
     }
 
 }
