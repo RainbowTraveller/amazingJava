a9e1e3581009a1659ad6bd6d7d0fcfb145c6910b
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-06-13 08:47:14 -0700

    COrrection of narrative

--- programs/practice/strings/WordLadder.java
+++ programs/practice/strings/WordLadder.java
@@ -13,7 +13,7 @@ import java.util.*;
     coneY
     Money
 
-   Word ladders were invented by Lewis Carroll in 1878, the author of Alice in Wonderland. A ladder is a sequence of words that starts at the starting word, ends at the ending word,
+   Word ladders were invented by Lewis Carroll in 1878, the author of Alice in Wonderland. A ladder is a sequence of words that has a starting and ending word
    In a word ladder puzzle you have to change one word into another by altering a single letter at each step. Each word in the ladder must be a valid English word, and must have the
    same length. For example, to turn stone into money, one possible ladder is given on the left. Many ladder puzzles have more than one possible solutions. Your program must determine
    a shortest word ladder. Another path from stone to money is
