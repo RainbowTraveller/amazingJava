075a3d689b45460ffe18babd741ec1a36ddb481b
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-06-13 08:46:51 -0700

    Compaction of logic

--- programs/practice/strings/SubListIndex.java
+++ programs/practice/strings/SubListIndex.java
@@ -71,13 +71,11 @@ public class SubListIndex {
 	private boolean findHelper(int startIndex) {
 		int list2Tracker = 1;
 		boolean result = false;
-		while(startIndex < list1.size() && list2Tracker < list2.size()) {
-			if(list1.get(startIndex) != list2.get(list2Tracker)) {
-				break;
-			}
+		while(startIndex < list1.size() && list2Tracker < list2.size() && list1.get(startIndex) == list2.get(list2Tracker)) {
 			startIndex++;
 			list2Tracker++;
 		}
+
 		if(list2Tracker == list2.size()) {
 			result = true;
 		}
