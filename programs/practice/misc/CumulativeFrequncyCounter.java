71165ec49abcf1581f6e8214e20e42cb29bd2a83
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-07-11 08:56:29 -0700

    Simpler version

--- programs/practice/misc/CumulativeFrequncyCounter.java
+++ programs/practice/misc/CumulativeFrequncyCounter.java
@@ -19,112 +19,31 @@ Expected output (in any order):
 
 public class CumulativeFrequncyCounter {
     public static void main(String[] args) {
-        String[] counts = {
-            "900,google.com",
-            "60,mail.yahoo.com",
-            "10,mobile.sports.yahoo.com",
-            "40,sports.yahoo.com",
-            "300,yahoo.com",
-            "10,stackoverflow.com",
-            "2,en.wikipedia.org",
-            "1,es.wikipedia.org" };
-/*
-        //Separate each given domain and the count
-        Map<String, Integer> inputSeparated = separateDomainAndCount( counts );
-        //Process each domain to obtain basic domains to look for
-        Set<String> domains = getDomains( inputSeparated.keySet() );
+        String[] counts = { "900,google.com",
+                            "60,mail.yahoo.com",
+                            "10,mobile.sports.yahoo.com",
+                            "40,sports.yahoo.com",
+                            "300,yahoo.com",
+                            "10,stackoverflow.com",
+                            "2,en.wikipedia.org",
+                            "1,es.wikipedia.org" };
 
-
-        //Search obtained basic domain in the input and check for presence
-        //add the frequency encountered
-        for( String uniqueDomain : domains ) {
-            int count = 0;
-            for(String key : inputSeparated.keySet()) {
-                if( key.contains( uniqueDomain ) ) {
-                    count += inputSeparated.get( key );
-                }
-            }
-            System.out.println( count + "  " + uniqueDomain );
-        }
-*/
-        /*
-         * Simple way of finding this out instead of above cumbersome way
-         */
-
-		Map<String, Integer> domains = new HashMap();
-        for(String domain : counts) {
-            //String[] split = domain.split("\\s+");
+        Map<String, Integer> domains = new HashMap();
+        for (String domain : counts) {
+            // String[] split = domain.split("\\s+");
             String[] split = domain.split(",");
 
             String candidate = split[1];
             int count = Integer.valueOf(split[0]);
             int dotIndex = 0;
-            while(dotIndex >= 0) {
+            while (dotIndex >= 0) {
                 String currDomain = candidate.substring(dotIndex == 0 ? dotIndex : dotIndex + 1);
                 domains.put(currDomain, domains.getOrDefault(currDomain, 0) + count);
                 dotIndex = candidate.indexOf('.', dotIndex + 1);
             }
         }
 
-        for (String dom: domains.keySet())
+        for (String dom : domains.keySet())
             System.out.println(" " + domains.get(dom) + " " + dom);
     }
-
-
-  /**
-   *  Consider all the input domains and create set of required domains
-   *  call helper function to process each candidate domain string
-   */
-  public static Set<String> getDomains( Set<String> keys ) {
-    Set<String> validDomains = new HashSet<String>();
-    for (String source : keys ) {
-        System.out.println("Current Input domain : " + source);
-        List<String> currentValidDomains = getCurrentValidDomains( source );
-        //Using streams and internal iterator
-        currentValidDomains.stream()
-            .forEach(validDomains::add);//Method reference
-    }
-    return validDomains;
-  }
-
-  /**
-   *  candidate: mail.yahoo.com
-   *  Output :  mail.yahoo.com
-   *            yahoo.com
-   *            com
-   */
-    public static List<String> getCurrentValidDomains( String singleSource ) {
-        List<String> domains = new ArrayList<String>();
-        int index  = 0;
-        while(index >= 0) {
-            if(index != 0) {
-                index++;
-            }
-            String currentDomain = singleSource.substring(index );
-            domains.add(currentDomain);
-            System.out.println("C D : " + currentDomain);
-            index = singleSource.indexOf(".", index  + 1);
-        }
-        return domains;
-  }
-
-  /**
-   * Separated the frequency from the name of the domain
-   * and stored them as key value pair
-   * where domain is the key and the value is the frequency
-   */
-  public static Map<String, Integer> separateDomainAndCount( String[] input ) {
-      Map<String, Integer> inputDomainToCount = new HashMap<String, Integer>();
-      for(String i : input ) {
-          String[] inputSplit = i.split(",");
-          if(inputSplit.length == 2) {
-              String domain = inputSplit[ 1 ];
-              int count = Integer.valueOf( inputSplit[ 0 ] );
-              System.out.println("Domain : " + domain + " -- " + "Count : " + count);
-              inputDomainToCount.put( domain, count );
-          }
-      }
-      return inputDomainToCount;
-  }
 }
-
