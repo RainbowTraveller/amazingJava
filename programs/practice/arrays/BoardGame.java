00782e1b2786cc5151b0b52cd87781c693472bb6
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-07-18 17:26:42 -0700

    Code copaction using simpler method

--- programs/practice/arrays/BoardGame.java
+++ programs/practice/arrays/BoardGame.java
@@ -36,8 +36,10 @@ class BoardGame {
         int[][] board2 = new int[][] { { 0, 0, 0, 0, -1 }, { 0, -1, -1, 0, 0 }, { 0, 0, 0, 0, 0 }, { -1, -1, 0, 0, 0 },
                 { 0, -1, 0, 0, 0 }, { 0, -1, 0, 0, 0 }, };
         int[] start = { 0, 0 };
-        System.out.println("Is Travel Possible ? : " + isTravelPossible(board1, start));
-        System.out.println("Is Travel Possible ? : " + isTravelPossible(board2, start));
+        System.out.println("Is Travel Possible ? : " + isTravelPossible(board1, start[0], start[1]));
+        System.out.println("Is Travel Possible ? : " + isTravelPossible(board2, start[0], start[1]));
+        System.out.println("Is Travel Possible ? : " + isVisitPossible(board1, start[0], start[1]));
+        System.out.println("Is Travel Possible ? : " + isVisitPossible(board2, start[0], start[1]));
     }
 
     static class Position {
@@ -66,11 +68,9 @@ class BoardGame {
         }
     }
 
-    public static List<Position> getPossibleMoves(int[][] board, int[] currentPosition) {
+    public static List<Position> getPossibleMoves(int[][] board, int currRow, int currColumn) {
         List<Position> possibleMoves = new LinkedList<Position>();
-        if (board != null && currentPosition != null) {
-            int currRow = currentPosition[0];
-            int currColumn = currentPosition[1];
+        if (board != null) {
             if (currRow >= 0 && currRow < board.length) {
                 // currRow, currColumn + 1
                 if ((currColumn + 1) >= 0 && (currColumn + 1) < board[0].length
@@ -97,27 +97,24 @@ class BoardGame {
         return possibleMoves;
     }
 
-    public static boolean isTravelPossible(int[][] board, int[] position) {
+    public static boolean isTravelPossible(int[][] board, int row, int col) {
 
         Stack<Position> possibles = new Stack<Position>();
         int count = 0;
         int validPosition = 0;
         Set<Position> visited = new HashSet<Position>();
 
-        if (position != null && board != null) {
+        if (board != null) {
 
-            Position startPoint = new Position(position[0], position[1]);
+            Position startPoint = new Position(row, col);
             possibles.push(startPoint);
             visited.add(startPoint);
             count++;
             while (!possibles.isEmpty()) {
                 Position curr = possibles.pop();
-                int[] currLocation = new int[2];
-                currLocation[0] = curr.row;
-                currLocation[1] = curr.column;
 
                 // Get all valid neighbouring points
-                List<Position> currMoves = getPossibleMoves(board, currLocation);
+                List<Position> currMoves = getPossibleMoves(board, curr.row, curr.column);
 
                 for (Position p : currMoves) {
                     // Push them to stack only if they are not already visited
@@ -139,5 +136,30 @@ class BoardGame {
         }
         System.out.println("Count : " + count + " Valid Positions : " + validPosition);
         return count == validPosition;
+    }
+
+    public static boolean isVisitPossible(int[][] board, int x, int y) {
+        int validPosition = 0;
+        int count = visitHelper(board, x ,y);
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[0].length; ++j) {
+                if (board[i][j] == 0 || board[i][j] == -2) {
+                    validPosition++;
+                }
+            }
+        }
+        System.out.println("Count : " + count + " Valid Positions : " + validPosition);
+        return count == validPosition;
+    }
+
+    public static int visitHelper(int[][] board, int x, int y) {
+        int count = 0;
+        if(board != null) {
+            if(x >= 0 && x < board.length && y >=0 && y < board[0].length && board[x][y] != -1 && board[x][y] != -2 ) {
+                board[x][y] = -2;
+                count =  1 + visitHelper(board, x + 1,y) + visitHelper(board, x - 1, y) + visitHelper(board, x, y + 1) + visitHelper(board, x, y - 1);
+            }
+        }
+        return count;
     }
 }
