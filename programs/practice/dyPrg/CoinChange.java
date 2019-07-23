645e045a167e1ef05baca46bfcbfbe9e93b3925b
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-07-22 19:36:57 -0700

    Compilation and formatting corrections

--- programs/practice/dyPrg/CoinChange.java
+++ programs/practice/dyPrg/CoinChange.java
@@ -49,7 +49,7 @@ public class CoinChange {
             */
 
             System.out.println("Highly Recursive and exhaustive : ");
-            System.out.println("Minimum Coins Required : " + getMinCoins(coins, amount, 0));
+            System.out.println("Minimum Coins Required : " + getMinCoinsRecursion(coins, amount, 0));
             System.out.println("Total Time to heavy recursion : " + (System.currentTimeMillis() - start ) / 1000);
 
             /*
@@ -98,28 +98,28 @@ public class CoinChange {
     }
 
 
-	/*
-	 * This is by far simplest approach but with depth first heavy recursion
-	 * We consider 1 coin of a denomination and immediately increase count by 1. Then
-	 * we go 1 level deeper with amount reduced by that much denomination.
-	 * 		If we hit 0 that means we can get finite no. of coins : We return count obtained so far
-	 * 		If we hit a negative no. which mean no solution possible through this path
-	 * When we reduce amount by certain denomination, we try all possible denominations and count
-	 * no. of coins required. We gather at each stage min. number and pass on to previous iteration.
-	 * Hence the final answer
-	 */
-	//With this method signature uncomment the inline code to have memoization version
-    //public static int getMinCoins(int[] coins, int amount, int index, int[] minCoins) {
-    public static int getMinCoins(int[] coins, int amount, int index) {
-		//Tracking min value at this stage
+    /*
+     * This is by far simplest approach but with depth first heavy recursion
+     * We consider 1 coin of a denomination and immediately increase count by 1. Then
+     * we go 1 level deeper with amount reduced by that much denomination.
+     *      If we hit 0 that means we can get finite no. of coins : We return count obtained so far
+     *      If we hit a negative no. which mean no solution possible through this path
+     * When we reduce amount by certain denomination, we try all possible denominations and count
+     * no. of coins required. We gather at each stage min. number and pass on to previous iteration.
+     * Hence the final answer
+     */
+    //With this method signature uncomment the inline code to have memoization version
+    //public static int getMinCoinsRecusion(int[] coins, int amount, int count, int[] minCoins) {
+    public static int getMinCoinsRecursion(int[] coins, int amount, int count) {
+        //Tracking min value at this stage
         int min = Integer.MAX_VALUE;
-        if(val == 0) {
-			//Count so far is correct so return
+        if(amount == 0) {
+            //Count so far is correct so return
             return count;
         }
 
-        if(val < 0) {
-			//No solution with this path
+        if(amount < 0) {
+            //No solution with this path
             return -1;
         }
 
@@ -130,24 +130,24 @@ public class CoinChange {
         //}
 
 
-		//Consider all the coins
-        for(int i = 0; i < c.length; ++i) {
-			//For each try deducing the amount and next possible options
-			//increment the no. of coins when considered
-            int curr = coins(c, val - c[i], count + 1);
+        //Consider all the coins
+        for(int i = 0; i < coins.length; ++i) {
+            //For each try deducing the amount and next possible options
+            //increment the no. of coins when considered
+            int curr = getMinCoinsRecursion(coins, amount - coins[i], count + 1);
             if(curr != -1) {
-				//If valid value is returned then get min out of them
+                //If valid value is returned then get min out of them
                 min = Math.min(min, curr);
             }
         }
-		//In some cases all values may lead to dead end
-		//hence the original min var value is not changed
+        //In some cases all values may lead to dead end
+        //hence the original min var value is not changed
         return min == Integer.MAX_VALUE ? -1 : min;
 
         //0 based index so we have amount - 1 as index
         //minCoins[ amount - 1 ] = (min == Integer.MAX_VALUE ? -1 : min);
         //return minCoins[ amount - 1 ];
-	}
+    }
 
     /*
      * This is top down approach. Here we can clearly see that the function is of the type F(S) = F( S - c ) + 1.
@@ -195,32 +195,33 @@ public class CoinChange {
     }
 
     public static int getMinCoins(int [] coins, int amount) {
-		//adjust array length
+        //adjust array length
         int max = amount + 1;
 
-		//tracking
+        //tracking
         int[] dp = new int[amount + 1];
-		//Filling array with max amount + !
+        //Filling array with max amount + !
         Arrays.fill(dp, max);
 
-		//initialise first place to 0
+        //initialise first place to 0
         dp[0] = 0;
-		//System.out.println("Array : " + Arrays.toString(dp));
-		//Consider all amounts from 1 to actual amount
+        //System.out.println("Array : " + Arrays.toString(dp));
+        //Consider all amounts from 1 to actual amount
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
-				//Check if coin denomination is less than or equal to amount
-				//if it is greater then we can not choose this coin for our solution
+                //Check if coin denomination is less than or equal to amount
+                //if it is greater then we can not choose this coin for our solution
                 if (coins[j] <= i) {
-					//Get this difference current amount ( i ) and current coin
-					//denomination ( coin[j] )
-					//Check if we have a good value at this position meaning if this denomination of
-					//difference is present and has figured out it's coins. If so it will require 1 more coins
-					//of current denomination to reach the sum
+                    //Get this difference current amount ( i ) and current coin
+                    //denomination ( coin[j] )
+                    //Check if we have a good value at this position meaning if this denomination of
+                    //difference is present and has figured out it's coins. If so it will require 1 more coins
+                    //of current denomination to reach the sum
                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
+        System.out.println("Array : " + Arrays.toString(dp));
         return dp[amount] > amount ? -1 : dp[amount];
     }
 }
