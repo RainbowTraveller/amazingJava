0c1cda85dcb4797ce7f8573c1d56c73106436324
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-07-24 20:14:32 -0700

    Formatting and modifications

--- programs/practice/misc/FindUniqueFilesInADirectory.java
+++ programs/practice/misc/FindUniqueFilesInADirectory.java
@@ -13,62 +13,60 @@ import java.util.HashMap;
 import java.util.Stack;
 
 public class FindUniqueFilesInADirectory {
-	public static void main( String [] args ) {
-		String path = args[0];
-		List<List<String>> sameFilePathGroups = getSameFilePaths(path);
-	}
+    public static void main( String [] args ) {
+        String path = args[0];
+        List<List<String>> sameFilePathGroups = getSameFilePaths(path);
+    }
 
-	private static boolean isDir(String path) {
-		return true;
-	}
+    private static boolean isDir(String path) {
+        return true;
+    }
 
-	private static ArrayList<String> listDir(String rootPath) {
-		return new ArrayList<String>();
-	}
+    private static ArrayList<String> listDir(String rootPath) {
+        return new ArrayList<String>();
+    }
 
-	private static String getCheckSum( String path ) {
-		return new String();
-	}
-	/**
-	 * Modification of depth first search
-	 */
-	public static List<List<String>> getSameFilePaths( String path ) {
-		if( path != null ) {
-			Stack<String> pathStack = new Stack<String>();
-			pathStack.push( path );
-			Map<String, List<String>> sameFileTracker = new HashMap<String, List<String>>();
-			while(!pathStack.isEmpty()) {
-				String currentPath = pathStack.pop();
-				if( isDir( currentPath ) ) {
-					ArrayList<String> currSubPaths = listDir( currentPath );
-					for( String eachPath : currSubPaths ) {
-						pathStack.push( eachPath );
-					}
-				} else {
-					processFile( currentPath, sameFileTracker );
-				}
-			}
-			List<List<String>> desiredList = new ArrayList<List<String>>();
-			for(String checkSum : sameFileTracker.keySet()){
-				List<String> sameFilesPath = sameFileTracker.get( checkSum );
-				desiredList.add(sameFilesPath);
-			}
-			return desiredList;
-		}
-		return null;
-	}
+    private static String getCheckSum( String path ) {
+        return new String();
+    }
+    /**
+     * Modification of depth first search
+     */
+    public static List<List<String>> getSameFilePaths( String path ) {
+        if( path != null ) {
+            Stack<String> pathStack = new Stack<String>();
+            pathStack.push( path );
+            //Map of Checksum to Path
+            //Checksum should be same for all the identical files
+            Map<String, List<String>> sameFileTracker = new HashMap<String, List<String>>();
+            while(!pathStack.isEmpty()) {
+                String currentPath = pathStack.pop();
+                if( isDir( currentPath ) ) {
+                    ArrayList<String> currSubPaths = listDir( currentPath );
+                    //Java 8 : for each and method reference
+                    currSubPaths.forEach(pathStack::push);
+                } else {
+                    processFile( currentPath, sameFileTracker );
+                }
+            }
+            List<List<String>> desiredList = new ArrayList<List<String>>();
+            for(String checkSum : sameFileTracker.keySet()){
+                List<String> sameFilesPath = sameFileTracker.get( checkSum );
+                desiredList.add(sameFilesPath);
+            }
+            return desiredList;
+        }
+        return null;
+    }
 
-	/**
-	 *	Calculate the checksum of the file and track against the checksum as
-	 *	as key. The value will be list of paths with same checksum
-	 */
-	private static void processFile( String filePath, Map<String, List<String>> sameFileTracker ) {
-		String currCheckSum = getCheckSum(filePath);
-		List<String> sameFilePaths = new ArrayList<String>();
-		if( sameFileTracker.containsKey(currCheckSum) ) {
-			sameFilePaths = sameFileTracker.get( currCheckSum );
-		}
-		sameFilePaths.add(filePath);
-		sameFileTracker.put(currCheckSum, sameFilePaths);
-	}
+    /**
+     *  Calculate the checksum of the file and track against the checksum as
+     *  as key. The value will be list of paths with same checksum
+     */
+    private static void processFile( String filePath, Map<String, List<String>> sameFileTracker ) {
+        String currCheckSum = getCheckSum(filePath);
+        List<String> sameFilePaths = sameFileTracker.getOrDefault(currCheckSum, new ArrayList<String>());
+        sameFilePaths.add(filePath);
+        sameFileTracker.put(currCheckSum, sameFilePaths);
+    }
 }
