2c25b35aa76c4f46af968a527ccaa5363b2ee587
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-07-25 16:32:41 -0700

    Combining 2 separate files for same problem

--- programs/practice/misc/BracketCombos.java
+++ programs/practice/misc/BracketCombos.java
@@ -34,11 +34,37 @@ public class BracketCombos {
         }
     }
 
+    public void helper(LinkedList<String> al, int leftRem, int rightRem, StringBuffer str) {
+        if (leftRem == 0 && rightRem == 0) {
+            System.out.println("::: STORED ::: " + str);
+            al.add(str.toString());
+        } else {
+            if (leftRem > 0) {
+                System.out.println("LEFT: " + leftRem + " " + rightRem + " " + str);
+                str.append('(');
+                helper(al, leftRem - 1, rightRem, str);
+                str.deleteCharAt(str.length() - 1);
+            }
+
+            if (rightRem > leftRem) {
+                System.out.println("\t\t\tRIGHT: " + leftRem + " " + rightRem + " " + str);
+                str.append(')');
+                helper(al, leftRem, rightRem - 1, str);
+                str.deleteCharAt(str.length() - 1);
+            }
+        }
+    }
     public static void main(String[] args) {
         String input = args[0];
         BracketCombos bc = new BracketCombos();
         List<String> result =  new LinkedList<String>();
         bc.processParenthesis(result, new StringBuffer(), 0,0, Integer.parseInt(input));
         System.out.println(result);
+
+        StringBuffer str = new StringBuffer();
+        int len = Integer.parseInt(input);
+        LinkedList<String> al = new LinkedList<String>();
+        bc.helper(al, len, len, str);
+        al.forEach(System.out::println);
     }
 }
