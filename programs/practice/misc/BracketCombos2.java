2b98eaad1653b050a8b72430fa434ebf9f8a0571
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-07-24 20:39:31 -0700

    Revamp logic and formatting

--- programs/practice/misc/BracketCombos2.java
+++ programs/practice/misc/BracketCombos2.java
@@ -1,41 +1,43 @@
+
+/* Given an integer indicating no. of pairs of brackets
+ * find all possible valid permutations of such pairs
+ */
 import java.util.ArrayList;
+import java.lang.StringBuffer;
 
-public class BracketCombos2{
+public class BracketCombos2 {
 
-    public void helper(ArrayList<String> al, int leftRem, int rightRem, char[] str, int count) {
-        if(leftRem < 0 || rightRem < leftRem) return;
-
-        if(leftRem == 0 && rightRem == 0) {
-            String s = String.copyValueOf(str);
-            System.out.println("::: STORED ::: " + String.copyValueOf(str));
-            al.add(s);
+    public void helper(ArrayList<String> al, int leftRem, int rightRem, StringBuffer str) {
+        if (leftRem == 0 && rightRem == 0) {
+            System.out.println("::: STORED ::: " + str);
+            al.add(str.toString());
         } else {
-            if(leftRem > 0) {
-                System.out.println("LEFT: " + leftRem + " " + rightRem + " " + count + String.copyValueOf(str));
-                str[count] = '(';
-                helper(al, leftRem - 1, rightRem,str, count+1);
+            if (leftRem > 0) {
+                System.out.println("LEFT: " + leftRem + " " + rightRem + " " + str);
+                str.append('(');
+                helper(al, leftRem - 1, rightRem, str);
+                str.deleteCharAt(str.length() - 1);
             }
 
-            if(rightRem > leftRem) {
-                System.out.println("\t\t\tRIGHT: " + leftRem + " " + rightRem + " " + count + String.copyValueOf(str));
-                str[count] = ')';
-                helper(al, leftRem, rightRem - 1,str, count+1);
+            if (rightRem > leftRem) {
+                System.out.println("\t\t\tRIGHT: " + leftRem + " " + rightRem + " " + str);
+                str.append(')');
+                helper(al, leftRem, rightRem - 1, str);
+                str.deleteCharAt(str.length() - 1);
             }
         }
     }
 
     public void paranthesisCombo(ArrayList<String> al, int len) {
-        char[] str = new char[len*2];
-        helper(al, len, len, str,0);
+        StringBuffer str = new StringBuffer();
+        helper(al, len, len, str);
     }
 
     public static void main(String[] args) {
         String input = args[0];
         BracketCombos2 bc = new BracketCombos2();
         ArrayList<String> al = new ArrayList<String>();
-        bc.paranthesisCombo(al,Integer.parseInt(input));
-        for(String s : al) {
-            System.out.println(s);
-        }
+        bc.paranthesisCombo(al, Integer.parseInt(input));
+        al.forEach(System.out::println);
     }
 }
