8c9e3aa6a957068ca1daa2655a1099050c50d098
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-07-30 16:59:12 -0700

    Added correct profile name

--- projects/messenger/src/main/java/com/rnrmedia/social/messenger/service/ProfileService.java
+++ projects/messenger/src/main/java/com/rnrmedia/social/messenger/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.rnrmedia.social.messenger.service;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Date;
 
 import com.rnrmedia.social.messenger.database.DatabaseUtil;
 import com.rnrmedia.social.messenger.model.Profile;
@@ -13,15 +14,14 @@ public class ProfileService {
 
 
     public ProfileService() {
-        profiles.put("Holmes", new Profile(1L, "Baker", "Sherlock", "Holmes"));
-        profiles.put("Bro", new Profile(2L, "Ebinburgh", "Mycroft", "Holmes"));
-        profiles.put("Enemy", new Profile(3L, "Cardiff", "Moriarty", "Debrah"));
+        profiles.put("genius", new Profile(1L, "genius", "Sherlock", "Holmes"));
+        profiles.put("bro", new Profile(2L, "bro", "Mycroft", "Holmes"));
+        profiles.put("enemy", new Profile(3L, "enemy", "Moriarty", "Debrah"));
     }
 
     public List<Profile> getAllProfiles() {
         return new LinkedList<Profile>(profiles.values());
     }
-
 
     public Profile getProfile(String profileName) {
         return profiles.get(profileName);
@@ -30,6 +30,7 @@ public class ProfileService {
 
     public Profile addProfile(Profile profile) {
         profile.setId(profiles.size() + 1);
+        profile.setCreated(new Date());
         profiles.put(profile.getProfileName(), profile);
         return profile;
     }
