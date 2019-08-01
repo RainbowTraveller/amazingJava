ecbfeb14be30a064a6b56716cbac076a66807110
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-08-01 16:58:00 -0700

    Add link to self in Message

--- projects/messenger/src/main/java/com/rnrmedia/social/messenger/resources/MessageResource.java
+++ projects/messenger/src/main/java/com/rnrmedia/social/messenger/resources/MessageResource.java
@@ -1,10 +1,10 @@
 package com.rnrmedia.social.messenger.resources;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,7 +13,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.rnrmedia.social.messenger.model.Message;
 import com.rnrmedia.social.messenger.resources.bean.MessageFilterBean;
@@ -50,8 +53,15 @@ public class MessageResource {
 
     @GET
     @Path("/{messageId}")
-    public Message getMessage(@PathParam("messageId") long messageId) {
-        return msgService.getMessage(messageId);
+    public Message getMessage(@PathParam("messageId") long messageId, @Context UriInfo uriInfo) {
+        Message message = msgService.getMessage(messageId);
+        String uri = uriInfo.getBaseUriBuilder()
+            .path(MessageResource.class)
+            .path(Long.toString(message.getId()))
+            .build()
+            .toString();
+        message.addLink(uri, "self");
+        return message;
     }
 
     @PUT
@@ -68,9 +78,11 @@ public class MessageResource {
     }
 
     @POST
-    public Response addMessage(Message message) {
+    public Response addMessage(Message message, @Context UriInfo uriInfo) throws URISyntaxException {
         Message newMessage = msgService.addMessage(message);
-        return Response.status(Status.CREATED).entity(newMessage).build();
+        String newId = String.valueOf(newMessage.getId());
+        URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
+        return Response.created(uri).entity(newMessage).build();
     }
 
     @Path("/{messageId}/comments")
