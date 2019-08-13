ca4858f9c6cce8241e40139fcb68e60550bb6474
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-08-13 07:24:11 -0700

    Comments modification

--- programs/practice/arrays/SubArraySumEqualsK.java
+++ programs/practice/arrays/SubArraySumEqualsK.java
@@ -38,8 +38,8 @@ public class SubArraySumEqualsK {
 /**
  *
 The idea behind this approach is as follows: If the cumulative sum(represented by sum[i] for sum upto ith
-index) upto two indices is the same, the sum of the elements lying in between those indices is zero. Extending the same thought further, if the cumulative sum upto two indices, say ii and jj is at a difference of kk
-i.e. if sum[i] - sum[j] = k, the sum of elements lying between indices ii and jj is kk.
+index) upto two indices is the same, the sum of the elements lying in between those indices is zero. Extending the same thought further, if the cumulative sum upto two indices, say i and j is at a difference of k
+i.e. if sum[i] - sum[j] = k, the sum of elements lying between indices i and j is k.
 
 Based on these thoughts, we make use of a hashmap which is used to store the cumulative sum upto all the indices possible along with the number of times the same sum occurs. We store the data in the form:
 (sum_i, no. of occurrences of sum_i). We traverse over the array nums and keep on finding the cumulative sum. Every time we encounter a new sum, we make a new entry in the hashmap corresponding to that sum.
