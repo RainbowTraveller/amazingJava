627dc1e72dc02eb1638c251a882cd8066f324f7f
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-08-22 15:50:12 -0700

    Java8 programming idioms

--- projects/Lambdas/src/main/java/com/practice/Person.java
+++ projects/Lambdas/src/main/java/com/practice/Person.java
@@ -1,45 +1,23 @@
-package com.practice;
-
-/**
- * Hello world!
- *
- */
 public class Person {
-    private String firstName;
-    private String lastName;
-    private int age;
-
-    public Person( String firstName, String lastName, int age ) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-    public void setFirstName( String fname ) {
-        this.firstName = fname;
-    }
-
-    public void setLastName( String lname ) {
-        this.lastName = lname;
-    }
-
-    public void setAge( int age ) {
-        this.age = age;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-
-    public String toString(  ) {
-        return "Person : age " + this.age + " First Name : " + this.firstName + " Last Name : " + this.lastName;
-    }
+  private final String name;
+  private final Gender gender;
+  private final int age;
+  
+  public Person(String theName, Gender theGender, int theAge) {
+    name = theName;
+    gender = theGender;
+    age = theAge;
+  }
+  
+  public String getName() { 
+    return name; 
+  }
+  public int getAge() { 
+    return age; 
+  }
+  public Gender getGender() { return gender; }
+  
+  public String toString() {
+    return String.format("%s -- %s -- %d", name, gender, age);
+  }  
 }
