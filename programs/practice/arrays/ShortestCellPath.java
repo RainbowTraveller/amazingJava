6fa6695ff462618cc2cd48549dd2298d944219c1
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-08-25 14:42:41 -0700

    BFS example

---
+++ programs/practice/arrays/ShortestCellPath.java
@@ -0,0 +1,101 @@
+
+/*
+*
+*  Shortest Cell Path
+*  In a given grid of 0s and 1s, we have some starting row and column sr, sc and a target row and column tr, tc. Return the length of the shortest path from sr, sc to tr, tc that walks along 1 values only.
+*
+*  Each location in the path, including the start and the end, must be a 1. Each subsequent location in the path must be 4-directionally adjacent to the previous location.
+*
+*  It is guaranteed that grid[sr][sc] = grid[tr][tc] = 1, and the starting and target positions are different.
+*
+*  If the task is impossible, return -1.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class ShortestCellPath {
+
+    static class Location {
+        int row;
+        int col;
+
+        public Location(int row, int col) {
+
+            this.row = row;
+            this.col = col;
+        }
+
+        public int hashcode() {
+            return row | (col << 15);
+        }
+
+        public boolean equals(Object o) {
+            if (o instanceof Location) {
+                Location curr = (Location) o;
+                return curr.row == this.row && curr.col == this.col;
+            }
+            return false;
+        }
+
+        public String toString() {
+            return " Row : " + row + " Col : " + col;
+        }
+
+    }
+
+    // Classic breadth first search, visiting all neighbors, if destination is found
+    // return the level or increment level or path value
+    static int shortestCellPath(int[][] grid, int sr, int sc, int tr, int tc) {
+        int shortestPath = 0;
+
+        if (grid != null) {
+            Deque<Location> queue = new LinkedList<>();
+            Location start = new Location(sr, sc);
+            queue.offer(start);
+            int[][] indexes = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                while (size > 0) {
+                    Location curr = queue.poll();
+                    int row = curr.row;
+                    int col = curr.col;
+                    grid[row][col] = -1;//Using grid ifself to keep track of visited
+
+                    if (row == tr && col == tc) {
+                        return shortestPath;
+                    }
+
+                    for (int i = 0; i < indexes.length; ++i) {
+                        int newRow = row + indexes[i][0];
+                        int newCol = col + indexes[i][1];
+                        if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                                && grid[newRow][newCol] != 0 && grid[newRow][newCol] != -1) {
+                            Location newLocation = new Location(newRow, newCol);
+                            queue.offer(newLocation);
+                        }
+                    }
+                    size--;
+                }
+                shortestPath++;
+            }
+        }
+        return -1;
+    }
+
+    public static void printGrid(int[][] grid) {
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                System.out.print(" " + grid[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = { { 1, 1, 1, 1 }, { 0, 0, 0, 1 }, { 1, 1, 1, 1 } };
+        System.out.println(shortestCellPath(grid, 0, 0, 2, 0));
+    }
+
+}
