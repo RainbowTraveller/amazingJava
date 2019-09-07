b9e60bdf5d2a853e87f83c21007facebf3ad176a
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-09-07 07:28:14 -0700

    Added integer converter method

--- programs/practice/strings/StrToLongConverter.java
+++ programs/practice/strings/StrToLongConverter.java
@@ -1,17 +1,18 @@
 import java.lang.NullPointerException;
 import java.lang.Exception;
+
 /*
  * Converts numeric string to long value
  *
  */
 public class StrToLongConverter {
-    private long result;//stores result
+    private long result;// stores result
     private int signIndex;// index for sign
-    private boolean isNegative;//flag to indicate -ve number
+    private boolean isNegative;// flag to indicate -ve number
 
-    private final int TEN = 10;//multiplying factor
-    private final char SIGN = '-';//-ve sign
-    private final char ZERO = '0';//base to calculate offset for a number
+    private final int TEN = 10;// multiplying factor
+    private final char SIGN = '-';// -ve sign
+    private final char ZERO = '0';// base to calculate offset for a number
 
     /*
      * Constructor
@@ -24,31 +25,32 @@ public class StrToLongConverter {
 
     /*
      * Converts given string to equivalent long number
+     *
      * @throws Nullpointerexception, Invalidinputexception
      */
     public long stringToLong(String str) throws NullPointerException, InvalidInputException {
         result = 0;
-        if(str != null) {
-            //Check for -ve number
-            if(str.charAt(signIndex) == SIGN) {
+        if (str != null) {
+            // Check for -ve number
+            if (str.charAt(signIndex) == SIGN) {
                 isNegative = true;
-                //get only number part of string excluding sign character
+                // get only number part of string excluding sign character
                 str = str.substring(signIndex + 1);
             }
 
-            for(char c : str.toCharArray()) {
+            for (char c : str.toCharArray()) {
                 result *= TEN;
-                //Deduce the number from its distance from ACSII value of zero
-                int diff = (int)c - (int)ZERO;
-                if(diff > 9) {
-                    //throw exception if string contains anything else than numbers
+                // Deduce the number from its distance from ACSII value of zero
+                int diff = (int) c - (int) ZERO;
+                if (diff > 9) {
+                    // throw exception if string contains anything else than numbers
                     throw new InvalidInputException("Input contains non numeric characters");
                 } else {
                     result += diff;
                 }
             }
-            if(isNegative == true) {
-                //adjustment for -ve number
+            if (isNegative == true) {
+                // adjustment for -ve number
                 result *= (-1);
             }
             return result;
@@ -56,61 +58,124 @@ public class StrToLongConverter {
         throw new NullPointerException("Input String is empty");
     }
 
+    public static int myAtoi(String str) {
+        boolean isNegative = false;
+
+        if (str == null || str.trim().isEmpty()) {
+            return 0;
+        }
+
+        str = str.trim();
+        char firstChar = str.charAt(0);
+        if (firstChar != '-' && firstChar != '+' && !Character.isDigit(firstChar)) {
+            return 0;
+        }
+
+        if (str.charAt(0) == '-') {
+            isNegative = true;
+        }
+        int index = 0;
+        if (str.charAt(0) == '-' || str.charAt(0) == '+') {
+            index = 1;
+        }
+
+        StringBuffer buff = new StringBuffer();
+
+        while (index < str.length()) {
+            char curr = str.charAt(index);
+            if (!Character.isDigit(curr)) {
+                break;
+            }
+            buff.append(curr);
+            index++;
+        }
+        int num = 0;
+        // System.out.println(buff);
+
+        for (int i = 0; i < buff.length(); i++) {
+            int currDigit = Character.getNumericValue(buff.charAt(i));
+
+            if (num > (Integer.MAX_VALUE - currDigit) / 10) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            // System.out.println(currDigit + " " + num);
+            num = (num * 10) + currDigit;
+        }
+
+        if (isNegative) {
+            num *= -1;
+        }
+        return num;
+    }
+
     /*
      * Exception used in case of invalid input string
      */
     @SuppressWarnings("serial")
     class InvalidInputException extends Exception {
-        InvalidInputException (String message) {
+        InvalidInputException(String message) {
             super(message);
-       }
+        }
     }
 
     public static void main(String[] args) {
         StrToLongConverter worker = new StrToLongConverter();
-            long testNumber = 0;
+        long testNumber = 0;
 
-            System.out.println("-------Testing implementation-------");
-            // Positive number
-            try {
-                testNumber = worker.stringToLong("123");
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            if (testNumber == 123) {
-                System.out.println("Success :" + testNumber);
-            }
+        System.out.println("-------Testing implementation-------");
+        // Positive number
+        try {
+            testNumber = worker.stringToLong("123");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if (testNumber == 123) {
+            System.out.println("Success :" + testNumber);
+        }
 
-            // Negative number
-            try {
-                testNumber = worker.stringToLong("-123");
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            if (testNumber == -123) {
-                System.out.println("Success :" + testNumber);
-            }
+        // Negative number
+        try {
+            testNumber = worker.stringToLong("-123");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if (testNumber == -123) {
+            System.out.println("Success :" + testNumber);
+        }
 
-            // Overflow
-            try {
-                testNumber = worker.stringToLong(Long.toString(Long.MAX_VALUE + 200));
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            System.out.println("Overflow Test :" + testNumber);
+        // Overflow
+        try {
+            testNumber = worker.stringToLong(Long.toString(Long.MAX_VALUE + 200));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Overflow Test :" + testNumber);
 
-            // Invalid number
-            try {
-                testNumber = worker.stringToLong("-ab3");
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+        // Invalid number
+        try {
+            testNumber = worker.stringToLong("-ab3");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
-            // Build in functionality check
-            System.out.println("-------Testing Built in function-------");
-            System.out.println(Long.parseLong("123"));
-            System.out.println(Long.parseLong("-123"));
-            System.out.println(Long.parseLong(Long.toString(Long.MAX_VALUE + 200)));
-            System.out.println(Long.parseLong("-a23"));
+        // Build in functionality check
+        System.out.println("-------Testing Built in function-------");
+        System.out.println(Long.parseLong("123"));
+        System.out.println(Long.parseLong("-123"));
+        System.out.println(Long.parseLong(Long.toString(Long.MAX_VALUE + 200)));
+        // System.out.println(Long.parseLong("-a23"));
+        // **************************************
+        // Converting to integer
+
+        System.out.println(myAtoi("42"));
+        System.out.println(myAtoi("-42"));
+        System.out.println(myAtoi("       42"));
+        System.out.println(myAtoi("       -42"));
+        System.out.println(myAtoi("4193 with words"));
+        System.out.println(myAtoi("words and 987"));
+        System.out.println(myAtoi("-91283472332"));
+        System.out.println(myAtoi("91283472332"));
+
     }
 }
