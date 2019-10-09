e1fde6c4ae2c87ed5d4dc577c1408859267c9810
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-10-09 09:37:01 -0700

    Formatting

--- programs/practice/threading/BlockingQueue.java
+++ programs/practice/threading/BlockingQueue.java
@@ -5,104 +5,105 @@ import java.util.NoSuchElementException;
 import java.util.concurrent.CountDownLatch;
 
 public class BlockingQueue {
-	public static void main(String[] args) {
-		// instantiate blocking queue
-		// instantiate a producer and consumer class
-		// then start threads to produce and consume events
-		//
-		//
-		CountDownLatch startSignal = new CountDownLatch(1);
-		// Shared Queue
-		LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<String>();
-		Producer eventProducer = new Producer(queue, startSignal);
-		Consumer eventConsumer = new Consumer(queue, startSignal);
+    public static void main(String[] args) {
+        // instantiate blocking queue
+        // instantiate a producer and consumer class
+        // then start threads to produce and consume events
+        //
+        //
+        CountDownLatch startSignal = new CountDownLatch(1);
+        // Shared Queue
+        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<String>();
+        Producer eventProducer = new Producer(queue, startSignal);
+        Consumer eventConsumer = new Consumer(queue, startSignal);
 
-		Thread pThread = new Thread(eventProducer);
-		pThread.start();
-		Thread cThread = new Thread(eventConsumer);
-		cThread.start();
-		startSignal.countDown();
-	}
+        Thread pThread = new Thread(eventProducer);
+        pThread.start();
+        Thread cThread = new Thread(eventConsumer);
+        cThread.start();
+        startSignal.countDown();
+    }
 }
 
 class Producer implements Runnable {
-	String[] events = {"Visit", "Visit", "Conversion", "Visit", "Conversion", "Visit", "Conversion", "Visit", "Visit", "Jupiter","Saturn","Neptune", "Jupiter","Saturn","Neptune","finish"};
-	private final LinkedBlockingQueue<String> pqueue;
-	private final CountDownLatch startSignal;
+    String[] events = { "Visit", "Visit", "Conversion", "Visit", "Conversion", "Visit", "Conversion", "Visit", "Visit",
+            "Jupiter", "Saturn", "Neptune", "Jupiter", "Saturn", "Neptune", "finish" };
+    private final LinkedBlockingQueue<String> pqueue;
+    private final CountDownLatch startSignal;
 
-	public Producer(LinkedBlockingQueue<String> pqueue, CountDownLatch startSignal) {
-		this.pqueue = pqueue;
-		this.startSignal = startSignal;
-	}
+    public Producer(LinkedBlockingQueue<String> pqueue, CountDownLatch startSignal) {
+        this.pqueue = pqueue;
+        this.startSignal = startSignal;
+    }
 
-	public void run() {
-		try {
-			startSignal.await();
-			produce();
-		} catch( InterruptedException ie ) {
-			Thread.currentThread().interrupt();
-		}
-	}
+    public void run() {
+        try {
+            startSignal.await();
+            produce();
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
-	private void produce() {
-	    for(String event: events) {
-			//Object is added to the shared queue
-			pqueue.add(event);
-	    }
-	}
+    private void produce() {
+        for (String event : events) {
+            // Object is added to the shared queue
+            pqueue.add(event);
+        }
+    }
 }
 
 class Consumer implements Runnable {
-	private final LinkedBlockingQueue<String> cqueue;
-	private Map<String, Integer> eventCountTracker;
-	private final CountDownLatch startSignal;
+    private final LinkedBlockingQueue<String> cqueue;
+    private Map<String, Integer> eventCountTracker;
+    private final CountDownLatch startSignal;
 
-	public Consumer(LinkedBlockingQueue<String> cqueue, CountDownLatch startSignal) {
-		this.cqueue = cqueue;
-		this.eventCountTracker = new HashMap<String, Integer>();
-		this.startSignal = startSignal;
-	}
+    public Consumer(LinkedBlockingQueue<String> cqueue, CountDownLatch startSignal) {
+        this.cqueue = cqueue;
+        this.eventCountTracker = new HashMap<String, Integer>();
+        this.startSignal = startSignal;
+    }
 
-	public void run() {
-		try {
-			startSignal.await();
-			while(cqueue.peek() != null) {
-				try{
-					String event = cqueue.take();
-					if(event.equalsIgnoreCase("finish")) {
-						break;
-					}
-					/*try {
-						Thread.sleep(10);
-					} catch(InterruptedException ie) {
-						System.out.println("Wait inturrupted");
-					}*/
-					consume(event);
-				} catch(InterruptedException ie) {
-					System.out.println("Take method interrupted");
-				}
-			}
-		} catch( InterruptedException ie ) {
-			Thread.currentThread().interrupt();
-		} catch (NoSuchElementException nsee) {
-		   System.out.println("Events exhausted");
-		} finally {
-			  result();
-		}
-	}
+    public void run() {
+        try {
+            startSignal.await();
+            while (cqueue.peek() != null) {
+                try {
+                    String event = cqueue.take();
+                    if (event.equalsIgnoreCase("finish")) {
+                        break;
+                    }
+                    /*
+                     * try { Thread.sleep(10); } catch(InterruptedException ie) {
+                     * System.out.println("Wait inturrupted"); }
+                     */
+                    consume(event);
+                } catch (InterruptedException ie) {
+                    System.out.println("Take method interrupted");
+                    Thread.currentThread().interrupt();
+                }
+            }
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        } catch (NoSuchElementException nsee) {
+            System.out.println("Events exhausted");
+        } finally {
+            result();
+        }
+    }
 
-	/*
-	 * Keeps track of the each string count
-	 */
-	private void consume(String event) {
-		int eventCount = eventCountTracker.getOrDefault(event, 0);
-		eventCountTracker.put(event,eventCount + 1);
-	}
+    /*
+     * Keeps track of the each string count
+     */
+    private void consume(String event) {
+        int eventCount = eventCountTracker.getOrDefault(event, 0);
+        eventCountTracker.put(event, eventCount + 1);
+    }
 
-	private void result() {
-		for(String event : eventCountTracker.keySet()) {
-			int count = eventCountTracker.get(event);
-			System.out.println(count + " " + event);
-		}
-	}
+    private void result() {
+        for (String event : eventCountTracker.keySet()) {
+            int count = eventCountTracker.get(event);
+            System.out.println(count + " " + event);
+        }
+    }
 }
