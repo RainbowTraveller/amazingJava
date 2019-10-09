4c8c583f995ad9c5caa1a02487380e2cfb583728
Milind Vaidya <MilindArvindVaidya@gmail.com>
2019-10-08 18:03:53 -0700

    Vital comment

--- programs/practice/strings/ReorderLogFiles.java
+++ programs/practice/strings/ReorderLogFiles.java
@@ -21,6 +21,8 @@ import java.util.Arrays;
 *
 *  Input: ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
 *  Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+*
+* Asked in Am : identifiers were alphanumeric and numeric instead only letters and digits
 */
 public class ReorderLogFiles {
     public static void main(String[] args) {
