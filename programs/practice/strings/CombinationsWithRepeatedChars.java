1c7e9013ea40e258b22e1e5372995be7c9a1e182
Milind Vaidya <milindv@uber.com>
2020-08-18 07:27:32 -0700

    Added important distinction for same problem but slightly modified case

--- programs/practice/strings/CombinationsWithRepeatedChars.java
+++ programs/practice/strings/CombinationsWithRepeatedChars.java
@@ -5,6 +5,8 @@
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
 
 public class CombinationsWithRepeatedChars {
     public static void combinations(char[] inputArr, StringBuffer output, int[] allowed, int starting) {
@@ -13,6 +15,9 @@ public class CombinationsWithRepeatedChars {
                 allowed[i]--;
                 output.append(inputArr[i]);
                 System.out.println(output.toString());
+                //Here we pass on i which is same as current index in the array
+                //This is because there are multiple occurrences of a particular character
+                //and if a character count is exhausted or not is checked by allowed variable
                 combinations(inputArr, output, allowed, i);
                 allowed[i]++;
                 output.deleteCharAt(output.length() - 1);
@@ -40,5 +45,32 @@ public class CombinationsWithRepeatedChars {
             i++;
         }
         combinations(inputArr, output, allowed, 0);
+    }
+
+    //This is subsets for set of unique integers, no repeated numbers
+    //idea remains the same
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (nums != null || nums.length > 0) {
+            helper(result, new LinkedList<Integer>(), 0, nums);
+            result.add(new LinkedList<Integer>());
+        }
+
+        return result;
+    }
+
+    public void helper(List<List<Integer>> result, LinkedList<Integer> curr, int i, int[] nums) {
+
+        for (int j = i; j < nums.length; ++j) {
+            curr.addLast(nums[j]);
+            result.add(new LinkedList<Integer>(curr));
+            //Here we are not dealing with multiple occurrences
+            //so there is not check of count of a number or character is
+            //exhausted. So adjust index to next character as j + 1``
+            helper(result, curr, j + 1, nums);
+
+            curr.removeLast();
+        }
+
     }
 }
