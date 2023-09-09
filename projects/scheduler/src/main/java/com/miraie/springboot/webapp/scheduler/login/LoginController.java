ae8a0556b5ae287689f97ed88726fdfc65de0f96
RainbowTraveller <milindarvindvaidya@gmail.com>
2023-09-08 18:28:06 -0700

    Request Param code without form : May be changed later

--- projects/scheduler/src/main/java/com/miraie/springboot/webapp/scheduler/login/LoginController.java
+++ projects/scheduler/src/main/java/com/miraie/springboot/webapp/scheduler/login/LoginController.java
@@ -1,17 +1,20 @@
 package com.miraie.springboot.webapp.scheduler.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+    //Adding logger
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String login(@RequestParam String name, ModelMap modelMap) {
         modelMap.put("username", name);
+        logger.debug("Request Param supplied {}", name);
         return  "login";
     }
 }
