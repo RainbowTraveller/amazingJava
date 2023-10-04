fc9251bb31f686049c8c59765a3e5677e7ca2707
RainbowTraveller <milindarvindvaidya@gmail.com>
2023-10-04 12:13:13 -0700

    New interface for DB operations

--- projects/scheduler/src/main/java/com/miraie/springboot/webapp/scheduler/task/TaskRepository.java
+++ projects/scheduler/src/main/java/com/miraie/springboot/webapp/scheduler/task/TaskRepository.java
@@ -1,2 +1,9 @@
-package com.miraie.springboot.webapp.scheduler.task;public interface TaskRepository {
+package com.miraie.springboot.webapp.scheduler.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    public List<Task> getByUserName (String userName);
 }
