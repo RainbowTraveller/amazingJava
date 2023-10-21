f3d33c02dda69957308ecaee4262f6ee8b4aa76a
RainbowTraveller <milindarvindvaidya@gmail.com>
2023-10-20 19:33:22 -0700

    Added okhttp and Twilio API clients

--- projects/RestClientApplication/src/main/java/com/rnrmedia/rest/client/TwillioHttpClient.java
+++ projects/RestClientApplication/src/main/java/com/rnrmedia/rest/client/TwillioHttpClient.java
@@ -6,11 +6,45 @@ import java.io.IOException;
 
 public class TwillioHttpClient {
     public static final String TWILIO_ACCOUNT_SID = "AC5a29821f9074ee80d32fc768da1afcca";
-    public static final String TWILIO_AUTH_TOKEN = "d785c9cb6a23179fd22a6d6221e31e6d";
+    public static final String TWILIO_AUTH_TOKEN = "AUTH";
+
+    private static OkHttpClient client;
 
     public static void main (String[] args) throws IOException {
+            //sendSMS();
+            listSMS();
 
-        OkHttpClient client = new OkHttpClient().newBuilder()
+    }
+
+
+    public static void sendSMS () throws IOException {
+        client = getClientWithAuth(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "Body=Hello World!&To={{+PHONE}}&From={{+18443867902}}");
+        Request request = new Request.Builder()
+//                .url("https://api.twilio.com/2010-04-01/Accounts/{{TWILIO_ACCOUNT_SID}}/Messages.json")
+                .url("https://api.twilio.com/2010-04-01/Accounts/" + TWILIO_ACCOUNT_SID + "/Messages.json")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Response response = client.newCall(request).execute();
+    }
+
+    public static void listSMS () throws IOException {
+
+        client = getClientWithAuth(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+        Request request = new Request.Builder()
+                .url("https://api.twilio.com/2010-04-01/Accounts/" + TWILIO_ACCOUNT_SID  + "/Messages.json")
+                .method("GET", null)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
+
+
+    private static OkHttpClient getClientWithAuth(String sid, String authToken) {
+        return new OkHttpClient().newBuilder()
                 .authenticator(((route, response) -> {
                     if (response.request().header("Authorization") != null) {
                         return null; // Give up, we've already attempted to authenticate.
@@ -25,14 +59,6 @@ public class TwillioHttpClient {
 
                 }))
                 .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "Application/json");
-        Request request = new Request.Builder()
-                .url("https://api.twilio.com/2010-04-01/Accounts/{{TWILIO_ACCOUNT_SID}}/AvailablePhoneNumbers/US/Local.json")
-                .method("GET", null)
-                .build();
-        Response response = client.newCall(request).execute();
-        System.out.println(response.body().toString());
 
     }
 }
--- projects/RestClientApplication/src/main/java/com/rnrmedia/rest/client/TwillioLibClient.java
+++ projects/RestClientApplication/src/main/java/com/rnrmedia/rest/client/TwillioLibClient.java
@@ -1,25 +1,34 @@
 package com.rnrmedia.rest.client;
 
 import com.twilio.Twilio;
-import com.twilio.converter.Promoter;
+import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-
-import java.net.URI;
-import java.math.BigDecimal;
-
 
 public class TwillioLibClient {
     // Find your Account Sid and Token at twilio.com/console
     public static final String ACCOUNT_SID = "AC5a29821f9074ee80d32fc768da1afcca";
-    public static final String AUTH_TOKEN = "d785c9cb6a23179fd22a6d6221e31e6d";
+    public static final String AUTH_TOKEN = "AUTH";
 
     public static void main (String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("+16127471828"),
-                new com.twilio.type.PhoneNumber("+18443867902"),
-                "Hey SMS Try".toString()).create();
-        System.out.println(message.getSid());
+        //Send SMS : Working with
+//        Message message = Message.creator(
+//                new com.twilio.type.PhoneNumber(toNumber),
+//                new com.twilio.type.PhoneNumber("+18443867902"),
+//                "Hey There, Howdie...!").create();
+//        System.out.println(message.getSid());
+
+        //Fetching all messages
+        ResourceSet<Message> messages = Message.reader().read();
+        /* not tested
+        // Fetch message for a given id
+        message = Message.fetcher(ACCOUNT_SID)
+                .fetch();
+        */
+
+        for(Message currMessage : messages) {
+            System.out.println(currMessage.getBody());
+        }
     }
+
 }
