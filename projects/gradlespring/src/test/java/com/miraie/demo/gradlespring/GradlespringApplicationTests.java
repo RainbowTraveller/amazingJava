9d468fc5da8718a3f3f70f4b11a2742a24d18b83
RainbowTraveller <milindarvindvaidya@gmail.com>
2024-02-28 15:43:19 -0800

    Brief gradle project for the record

---
+++ projects/gradlespring/src/test/java/com/miraie/demo/gradlespring/GradlespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miraie.demo.gradlespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
