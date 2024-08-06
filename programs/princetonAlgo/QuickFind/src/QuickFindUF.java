4b97aa82c87b2254355b29150cd3077ed8c310fd
RainbowTraveller <milindarvindvaidya@gmail.com>
2024-08-06 11:34:43 -0700

    Tried enabling formatting plugin

--- programs/princetonAlgo/QuickFind/src/QuickFindUF.java
+++ programs/princetonAlgo/QuickFind/src/QuickFindUF.java
@@ -2,73 +2,79 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Part 1 on the Union Find discussion in terms of dynamic connectivity
- * Given a set of N objects.
- * ・Union command: connect two objects.
- * ・Find/connected query: is there a path connecting the two objects?
+ * Part 1 on the Union Find discussion in terms of dynamic connectivity Given a set of N objects.
+ * ・Union command: connect two objects. ・Find/connected query: is there a path connecting the two
+ * objects?
  *
- * This is greedy or eager approach, where the invariant is as follows
- * If two nodes are connected to each other the values at that index in the backup array DS is same
+ * <p>This is greedy or eager approach, where the invariant is as follows If two nodes are connected
+ * to each other the values at that index in the backup array DS is same
  */
 public class QuickFindUF {
-    // Data structure mapping the nodes. This is array of n nodes which is indexed from 0 to n - 1
-    int [] nodes;
-    public static void main(String[] args) {
-        int n = StdIn.readInt();
-        QuickFindUF quickFindUF = new QuickFindUF(n);
-        while(!StdIn.isEmpty()) {
-            int p = StdIn.readInt();
-            int q = StdIn.readInt();
-            if(!quickFindUF.isConnected(p, q)) {
-                quickFindUF.union(p, q);
-                StdOut.println(p + " : " + q);
-            }
-        }
-    }
+  // Data structure mapping the nodes. This is array of n nodes which is indexed from 0 to n - 1
+  int[] nodes;
 
-    /**
-     * For implementation of Quick Find Algorithm for Union Find exercise we need to consider n nodes
-     * that we want to consider and experiment with connectivity
-     * @param n no. of nodes in the problem domain
-     */
-    public QuickFindUF(int n) {
-        nodes = new int[n];
-        //To begin with the array is initialized with index value of the node
-        for(int i = 0; i < n; i++) {
-            nodes[i] = i;
-        }
+  public static void main(String[] args) {
+    int n = StdIn.readInt();
+    QuickFindUF quickFindUF = new QuickFindUF(n);
+    while (!StdIn.isEmpty()) {
+      int p = StdIn.readInt();
+      int q = StdIn.readInt();
+      if (!quickFindUF.isConnected(p, q)) {
+        quickFindUF.union(p, q);
+        StdOut.println(p + " : " + q);
+      }
     }
+  }
 
-    /**
-     * Checks if two nodes are connected to each other. While doing union we make sure that two connected nodes have same
-     * value in the backed up DS at node's index
-     * @param from index of the source node
-     * @param to index of the destination node
-     * @return true if there is a connected path from node 'from' to the node 'to'
-     */
-    public boolean isConnected(int from, int to) {
-        return nodes[from] == nodes[to];
+  /**
+   * For implementation of Quick Find Algorithm for Union Find exercise we need to consider n nodes
+   * that we want to consider and experiment with connectivity
+   *
+   * @param n no. of nodes in the problem domain
+   */
+  public QuickFindUF(int n) {
+    nodes = new int[n];
+    // To begin with the array is initialized with index value of the node
+    for (int i = 0; i < n; i++) {
+      nodes[i] = i;
     }
+  }
 
-    /**
-     * Makes sure point from is connected to point to. This is achieved by replacing all the node values in the backing
-     * DS with value of to maintaining invariant that all connected nodes have same value in the backed up array DS
-     *
-     * Running complexity : As can be seen, the algorithm is named Quick Find, indicating the find operation is faster
-     * The union on the other hand needs entire array access for establishing invariant. So for N union commands, one
-     * for each node, N array scans are required, leading to N * N complexity
-     * @param from index of the source node
-     * @param to index of the destination node
-     */
-    public void union(int from, int to) {
-        if(nodes[from] != nodes[to]) {
-            int toId = nodes[to];
-            int fromId = nodes[from];
-            for(int i = 0; i < nodes.length; ++i) {
-                if(nodes[i] == fromId) {
-                    nodes[i] = toId;
-                }
-            }
+  /**
+   * Checks if two nodes are connected to each other. While doing union we make sure that two
+   * connected nodes have same value in the backed up DS at node's index
+   *
+   * @param from index of the source node
+   * @param to index of the destination node
+   * @return true if there is a connected path from node 'from' to the node 'to'
+   */
+  public boolean isConnected(int from, int to) {
+    return nodes[from] == nodes[to];
+  }
+
+  /**
+   * Makes sure point from is connected to point to. This is achieved by replacing all the node
+   * values in the backing DS with value of to maintaining invariant that all connected nodes have
+   * same value in the backed up array DS
+   *
+   * <p>Running complexity : As can be seen, the algorithm is named Quick Find, indicating the find
+   * operation is faster The union on the other hand needs entire array access for establishing
+   * invariant. So for N union commands, one for each node, N array scans are required, leading to N
+   * * N complexity
+   *
+   * @param from index of the source node
+   * @param to index of the destination node
+   */
+  public void union(int from, int to) {
+    if (nodes[from] != nodes[to]) {
+      int toId = nodes[to];
+      int fromId = nodes[from];
+      for (int i = 0; i < nodes.length; ++i) {
+        if (nodes[i] == fromId) {
+          nodes[i] = toId;
         }
+      }
     }
+  }
 }
+
--- programs/princetonAlgo/QuickUnion/src/QuickUnionUF.java
+++ programs/princetonAlgo/QuickUnion/src/QuickUnionUF.java
@@ -1,81 +1,86 @@
 /**
- * Part 2 on the Union Find discussion in terms of dynamic connectivity
- * Given a set of N objects.
- * ・Union command: connect two objects.
- * ・Find/connected query: is there a path connecting the two objects?
+ * Part 2 on the Union Find discussion in terms of dynamic connectivity Given a set of N objects.
+ * ・Union command: connect two objects. ・Find/connected query: is there a path connecting the two
+ * objects?
  *
- * This is lazy approach, where the invariant is as follows
- * 1. nodes[i] points to the index of the node which is root of this node at index i
- * 2. If two nodes are connected to each other, then they have same root i.e. if p and q are connected then nodes[p] and
- * nodes[q] have same value
+ * <p>This is lazy approach, where the invariant is as follows 1. nodes[i] points to the index of
+ * the node which is root of this node at index i 2. If two nodes are connected to each other, then
+ * they have same root i.e. if p and q are connected then nodes[p] and nodes[q] have same value
  */
 public class QuickUnionUF {
-    int [] nodes;
-    public static void main(String[] args) {
-        int n = StdIn.readInt();
-        QuickUnionUF quickUnionUF = new QuickUnionUF(n);
-        while(!StdIn.isEmpty()) {
-            int p = StdIn.readInt();
-            int q = StdIn.readInt();
-            if(!quickUnionUF.isConnected(p, q)) {
-                quickUnionUF.union(p, q);
-                StdOut.println(p + " : " + q);
-            }
-        }
-    }
+  int[] nodes;
 
-    /**
-     * For implementation of Quick Union Algorithm for Union Find exercise we need to consider n nodes
-     * that we want to consider and experiment with connectivity
-     * @param n no. of nodes in the problem domain
-     */
-    public QuickUnionUF(int n) {
-        nodes = new int[n];
-        for(int i = 0; i < n; ++i) {
-            nodes[i] = i;
-        }
+  public static void main(String[] args) {
+    int n = StdIn.readInt();
+    QuickUnionUF quickUnionUF = new QuickUnionUF(n);
+    while (!StdIn.isEmpty()) {
+      int p = StdIn.readInt();
+      int q = StdIn.readInt();
+      if (!quickUnionUF.isConnected(p, q)) {
+        quickUnionUF.union(p, q);
+        StdOut.println(p + " : " + q);
+      }
     }
+  }
 
-    /**
-     * Helper function for finding the root. It just traces the array one level at a time and returns a node where the
-     * index value is same as the value at the index, indicating the root node
-     * @param index this is the node for which the root node needs to be found
-     * @return index of the root node
-     */
-    private int getRoot(int index) {
-       while(index < nodes.length && index != nodes[index]) {
-          index = nodes[index];
-       }
-       return nodes[index];
+  /**
+   * For implementation of Quick Union Algorithm for Union Find exercise we need to consider n nodes
+   * that we want to consider and experiment with connectivity
+   *
+   * @param n no. of nodes in the problem domain
+   */
+  public QuickUnionUF(int n) {
+    nodes = new int[n];
+    for (int i = 0; i < n; ++i) {
+      nodes[i] = i;
     }
+  }
 
-    /**
-     * Makes sure point from is connected to point to. This is achieved by replacing all the node values in the backing
-     * DS with value of to maintaining invariant that all connected nodes have same value in the backed up array DS. The
-     * key difference here is that the value at an index is the index of the root node.
-     * Running complexity : As can be seen, the algorithm is named Quick Union, indicating the Union operation is faster
-     * Worst case the tree will be linear and then the traversal will be from leaf to root to fine the root. So it will
-     * be linear time N
-     * @param p index of the source node
-     * @param q index of the destination node
-     */
-    public void union(int p, int q) {
-        int pRoot = getRoot(p);
-        int qRoot = getRoot(q);
-        nodes[pRoot] = qRoot;
+  /**
+   * Helper function for finding the root. It just traces the array one level at a time and returns
+   * a node where the index value is same as the value at the index, indicating the root node
+   *
+   * @param index this is the node for which the root node needs to be found
+   * @return index of the root node
+   */
+  private int getRoot(int index) {
+    while (index < nodes.length && index != nodes[index]) {
+      index = nodes[index];
     }
+    return nodes[index];
+  }
 
-    /**
-     * Checks if two nodes are connected to each other. While doing union we make sure that two connected nodes have same
-     * root eventually.To achieve this we need to fetch the roots for both the nodes and then assign one to other
-     * So in this case we fetch the roots for both the nodes and then compare them
-     * @param p index of the source node
-     * @param q index of the destination node
-     * @return true if there is a connected path from node 'from' to the node 'to'
-     */
-    public boolean isConnected(int p, int q) {
-       int pRoot = getRoot(p);
-       int qRoot = getRoot(q);
-       return pRoot == qRoot;
-    }
+  /**
+   * Makes sure point from is connected to point to. This is achieved by replacing all the node
+   * values in the backing DS with value of to maintaining invariant that all connected nodes have
+   * same value in the backed up array DS. The key difference here is that the value at an index is
+   * the index of the root node. Running complexity : As can be seen, the algorithm is named Quick
+   * Union, indicating the Union operation is faster Worst case the tree will be linear and then the
+   * traversal will be from leaf to root to fine the root. So it will be linear time N
+   *
+   * @param p index of the source node
+   * @param q index of the destination node
+   */
+  public void union(int p, int q) {
+    int pRoot = getRoot(p);
+    int qRoot = getRoot(q);
+    nodes[pRoot] = qRoot;
+  }
+
+  /**
+   * Checks if two nodes are connected to each other. While doing union we make sure that two
+   * connected nodes have same root eventually.To achieve this we need to fetch the roots for both
+   * the nodes and then assign one to other So in this case we fetch the roots for both the nodes
+   * and then compare them
+   *
+   * @param p index of the source node
+   * @param q index of the destination node
+   * @return true if there is a connected path from node 'from' to the node 'to'
+   */
+  public boolean isConnected(int p, int q) {
+    int pRoot = getRoot(p);
+    int qRoot = getRoot(q);
+    return pRoot == qRoot;
+  }
 }
+
