63837e4798d2e3470ee7d1a22f776722ca0eb04e
RainbowTraveller <milindarvindvaidya@gmail.com>
2025-08-23 08:54:34 -0700

    Formatting Life and added O(n) solution for LongestConsecutiveSequence

--- programs/practice/arrays/Life.java
+++ programs/practice/arrays/Life.java
@@ -1,5 +1,6 @@
 /*
- *  According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+ *  According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in
+ *  1970."
  *  Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal)
  *  using the following four rules (taken from the above Wikipedia article):
  *
@@ -10,77 +11,77 @@
  *
  * Write a function to compute the next state (after one update) of the board given its current state.
  * The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
-*/
+ */
 
 public class Life {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Life l = new Life();
-        int[][] board = { { 0, 1, 0 }, { 0, 0, 1 }, { 1, 1, 1 }, { 0, 0, 0 } };
+    Life l = new Life();
+    int[][] board = {{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}};
 
-        l.gameOfLife(board);
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
+    l.gameOfLife(board);
+    for (int i = 0; i < board.length; ++i) {
+      for (int j = 0; j < board[0].length; ++j) {
+        System.out.print(board[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  /*
+   * This is in place logic, when we modify a cell, we mark it either 2 if it
+   * changes from 0 to 1 or mark it -2 if it changes from 1 to 0. This value is
+   * also checked in the helper function so that we can apply logic to all cells
+   * depending on there original neighbors and not after modification.
+   *
+   * Running : O(m * n) Space : O(1)
+   */
+  public void gameOfLife(int[][] board) {
+    for (int i = 0; i < board.length; ++i) {
+      for (int j = 0; j < board[0].length; ++j) {
+        int liveCount = getLiveNeighbors(i, j, board);
+        if (board[i][j] == 0 && liveCount == 3) {
+          board[i][j] = 2;
+        } else if (board[i][j] == 1 && (liveCount < 2 || liveCount > 3)) {
+          board[i][j] = -2;
         }
+      }
     }
 
+    for (int i = 0; i < board.length; ++i) {
+      for (int j = 0; j < board[0].length; ++j) {
+        if (board[i][j] == -2) {
+          board[i][j] = 0;
+        } else if (board[i][j] == 2) {
+          board[i][j] = 1;
+        }
+      }
+    }
+  }
+
+  /*
+   * Get all the neighbors with having positive value or are alive
+   */
+  private int getLiveNeighbors(int x, int y, int[][] board) {
+    int cnt = 0;
     /*
-     * This is in place logic, when we modify a cell, we mark it either 2 if it
-     * changes from 0 to 1 or mark it -2 if it changes from 1 to 0. This value is
-     * also checked in the helper function so that we can apply logic to all cells
-     * depending on there original neighbors and not after modification.
-     *
-     * Running : O(m * n) Space : O(1)
+     * Handy logic to visit all 8 neighbors
      */
-    public void gameOfLife(int[][] board) {
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
-                int liveCount = getLiveNeighbors(i, j, board);
-                if (board[i][j] == 0 && liveCount == 3) {
-                    board[i][j] = 2;
-                } else if (board[i][j] == 1 && (liveCount < 2 || liveCount > 3)) {
-                    board[i][j] = -2;
-                }
-            }
+    for (int i = -1; i < 2; ++i) {
+      for (int j = -1; j < 2; ++j) {
+        // Check it not out of bound
+        if (x + i >= 0 && x + i < board.length && y + j >= 0 && y + j < board[0].length) {
+          if (board[x + i][y + j] == 1 || board[x + i][y + j] == -2) {
+            cnt++;
+          }
         }
-
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
-                if (board[i][j] == -2) {
-                    board[i][j] = 0;
-                } else if (board[i][j] == 2) {
-                    board[i][j] = 1;
-                }
-            }
-        }
+      }
     }
 
-    /*
-     * Get all the neighbors with having positive value or are alive
-     */
-    private int getLiveNeighbors(int x, int y, int[][] board) {
-        int cnt = 0;
-        /*
-         * Handy logic to visit all 8 neighbors
-         */
-        for (int i = -1; i < 2; ++i) {
-            for (int j = -1; j < 2; ++j) {
-                // Check it not out of bound
-                if (x + i >= 0 && x + i < board.length && y + j >= 0 && y + j < board[0].length) {
-                    if (board[x + i][y + j] == 1 || board[x + i][y + j] == -2) {
-                        cnt++;
-                    }
-                }
-            }
-        }
-
-        // This is counted as well so decrement count by 1
-        if (board[x][y] == 1) {
-            cnt -= 1;
-        }
-        return cnt;
+    // This is counted as well so decrement count by 1
+    if (board[x][y] == 1) {
+      cnt -= 1;
     }
+    return cnt;
+  }
 }
--- programs/practice/arrays/LongestConsecutiveSequence.java
+++ programs/practice/arrays/LongestConsecutiveSequence.java
@@ -17,7 +17,7 @@ Output: 9
 
 NOTE :
 The aim is to find sequence of numbers which may be anywhere in the original input array but when sorted
-form longest sequence of consecutive numbers. This is not same as increasing, the word consicutive is important
+form longest sequence of consecutive numbers. This is not same as increasing, the word consecutive is important
  */
 
 public class LongestConsecutiveSequence {
@@ -25,31 +25,74 @@ public class LongestConsecutiveSequence {
     System.out.println(longestConsecutive(new int[] {100, 4, 200, 1, 3, 2}));
   }
 
+  /**
+   * Sort the array and then check for consecutive numbers. Time complexity : O(n log n) due to
+   * sorting. Space complexity : O(1).
+   */
   public static int longestConsecutive(int[] nums) {
     if (nums.length == 0) {
       return 0;
     }
-    //Sort
+    // Sort
     Arrays.sort(nums);
     int longestStreak = 1;
     int currStreak = 1;
-    //Start from 1 as we need to compare with previous no. in array.
+    // Start from 1 as we need to compare with previous no. in array.
     for (int i = 1; i < nums.length; ++i) {
-      //Check if adjacent nos are not same
+      // Check if adjacent nos are not same
       if (nums[i] != nums[i - 1]) {
-        //Check if they differ by 1
+        // Check if they differ by 1
         if (nums[i] == nums[i - 1] + 1) {
-          //if consecutive keep increasing current streak count
+          // if consecutive keep increasing current streak count
           currStreak++;
         } else {
-          //if not select longest one based on what we have so far
+          // if not select longest one based on what we have so far
           longestStreak = Math.max(longestStreak, currStreak);
           currStreak = 1;
         }
       }
     }
-    //Do not forget to check for last streak obtained
+    // Do not forget to check for last streak obtained
     longestStreak = Math.max(longestStreak, currStreak);
     return longestStreak;
+  }
+
+  /**
+   * Use a set to store unique numbers. Then for each number check if it is the start of a sequence
+   * by checking if num-1 is not present in the set. If it is the start of a sequence, keep checking
+   * for next consecutive numbers and keep increasing current streak count. <br>
+   * Time complexity : O(n) Space complexity : O(n)
+   *
+   * @param nums input array
+   */
+  public static int longestConsecutiveLinear(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    // Use a set to store unique numbers
+    Set<Integer> numSet = new HashSet<>();
+    // 1. Add all numbers to a HashSet for O(1) average time complexity lookups.
+    for (int num : nums) {
+      numSet.add(num);
+    }
+    int longestStreak = 0;
+    int currStreak = 0;
+    // 2. Iterate through each number in the original array.
+    for (int num : numSet) {
+      // 3. Check if the current number is the start of a sequence.
+      // A number is the start of a sequence if num - 1 does not exist in the set.
+      // This is the key to achieving O(n) time complexity, as it ensures we only
+      // process each potential sequence's starting point once.
+      if (!numSet.contains(num - 1)) {
+        currStreak = 1;
+        int currNum = num;
+        // Check for next consecutive numbers
+        while (numSet.contains(num + 1)) {
+          currStreak++;
+          currNum++;
+        }
+        longestStreak = Math.max(longestStreak, currStreak);
+      }
+    }
   }
 }
