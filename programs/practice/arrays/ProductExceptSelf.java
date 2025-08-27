6907d2884bb61d5bed7b4ab32a78ebe7e1dbe6f5
RainbowTraveller <milindarvindvaidya@gmail.com>
2025-08-27 06:31:54 -0700

    Formatting

--- programs/practice/arrays/ProductExceptSelf.java
+++ programs/practice/arrays/ProductExceptSelf.java
@@ -5,42 +5,47 @@
  */
 public class ProductExceptSelf {
 
-    public static void main(String[] args) {
-        int[] arr = { 1,2,3,4 };
-		printArray(arr);
-        getResult( arr );
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4};
+    printArray(arr);
+    getResult(arr);
+  }
+
+  // Program to find product of array except self
+  // Time Complexity : O(n)
+  // Space Complexity : O(1) if we don't consider output array
+  public static void getResult(int[] nums) {
+    if (nums != null) {
+      int length = nums.length;
+      // Final result array
+      int[] result = new int[length];
+      // key variable to store product of elements
+      int key = 1;
+      // Go left to right,each index contains product of all its
+      // previous numbers
+      for (int i = 0; i < length; ++i) {
+        result[i] = key;
+        key *= nums[i];
+      }
+
+      // last elements is perfectly calculated as in
+      // it contains product of all elements previous to
+      // it but not itself
+      key = 1;
+      // Go right to left, modify result array using elements from
+      // original num array R to L
+      for (int i = length - 1; i >= 0; --i) {
+        result[i] *= key;
+        key *= nums[i];
+      }
+      printArray(result);
     }
+  }
 
-    public static void getResult( int[] nums) {
-        if( nums != null ){
-			int length = nums.length;
-			int [] result = new int[length];
-			int key = 1;
-			//Go left to right,each index contains product of all its
-			//previous numbers
-			for(int i=0; i < length; ++i) {
-				result[i] = key;
-				key *= nums[i];
-			}
-
-			//last elements is perfectly calculated as in
-			//it contains product of all elements previous to
-			//it but not itself
-			key = 1;
-			//Go right to left, modify result array using elements from
-			//original num array R to L
-			for(int i = length - 1; i >= 0; --i) {
-				result[i] *= key;
-				key *= nums[i];
-			}
-			printArray(result);
-        }
+  public static void printArray(int[] arr) {
+    for (Integer i : arr) {
+      System.out.print(i + " ");
     }
-
-	public static void printArray( int[] arr) {
-		for(Integer i : arr) {
-			System.out.print( i + " " );
-		}
-		System.out.println();
-	}
+    System.out.println();
+  }
 }
