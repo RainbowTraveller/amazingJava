e14f41fc1684cb55695749ebef1b6be580fe91dd
RainbowTraveller <milindarvindvaidya@gmail.com>
2025-08-28 06:20:00 -0700

    Added new method and formatting

--- programs/practice/arrays/ShortestDistanceWords.java
+++ programs/practice/arrays/ShortestDistanceWords.java
@@ -25,53 +25,91 @@
 
 // This is solution for all the above 3 problems combined
 public class ShortestDistanceWords {
-    // Storing the input words
-    String [] words;
+  // Storing the input words
+  String[] words;
 
-	public static void main( String [] args ) {
+  public static void main(String[] args) {
 
-		//String[] words = { "a", "b","c", "d",  "a", "b","c", "d",  "a", "b","c", "d",  "a", "b","c", "d",  "a", "b","c", "d",  "a", "b","c", "d","a", "k", "d" };
-		//String[] words = {"a", "b", "c", "d"};
-        String[] words = {"practice", "makes", "perfect", "coding", "makes"};
-		ShortestDistanceWords sdw = new ShortestDistanceWords(words);
-		System.out.println( "Min Distance : makes, coding : " + sdw.shortestWordDistance("makes", "coding"));
-		System.out.println( "Min Distance : makes, makes : " + sdw.shortestWordDistance("makes", "makes"));
-		System.out.println( "Min Distance : practice, coding : " + sdw.shortestWordDistance("practice", "coding"));
-	}
+    // String[] words = { "a", "b","c", "d",  "a", "b","c", "d",  "a", "b","c", "d",  "a", "b","c",
+    // "d",  "a", "b","c", "d",  "a", "b","c", "d","a", "k", "d" };
+    // String[] words = {"a", "b", "c", "d"};
+    String[] words = {"practice", "makes", "perfect", "coding", "makes"};
+    ShortestDistanceWords sdw = new ShortestDistanceWords(words);
+    System.out.println(
+        "Min Distance : makes, coding : " + sdw.shortestWordDistance("makes", "coding"));
+    System.out.println(
+        "Min Distance : makes, makes : " + sdw.shortestWordDistance("makes", "makes"));
+    System.out.println(
+        "Min Distance : practice, coding : " + sdw.shortestWordDistance("practice", "coding"));
+  }
 
-    public ShortestDistanceWords( String[] words) {
-        this.words = words;
-    }
+  public ShortestDistanceWords(String[] words) {
+    this.words = words;
+  }
 
-    public int shortestWordDistance(String word1, String word2) {
-        int w1 = -1, w2 = -1;
-        int distance = Integer.MAX_VALUE;
-        int index = 0;
-        int oldIndex  = -1;
+  /**
+   * This method finds the shortest distance between two words in the array.
+   * It handles the case when both words are same and when they are different.
+   *
+   * @param word1 First word
+   * @param word2 Second word
+   * @return Shortest distance between the two words
+   */
+  public int shortestWordDistance(String word1, String word2) {
+    int w1 = -1, w2 = -1;
+    int distance = Integer.MAX_VALUE;
+    int index = 0;
+    int oldIndex = -1;
 
-        for(String word : words) {
-            if(!word1.equals(word2)) {
-                if(word.equals(word1)) {
-                    w1 = index;
-                } else if(word.equals(word2)) {
-                    w2 = index;
-                }
-
-                if(w1 >= 0  && w2 >=0) {
-                    distance = Math.min(distance, Math.abs(w1-w2));
-                }
-            } else {
-                if(word.equals(word1)) {
-                    if(oldIndex < 0) {
-                        oldIndex = index;
-                    } else {
-                        distance = Math.min(distance, Math.abs(oldIndex - index));
-                        oldIndex = index;
-                    }
-                }
-            }
-            index++;
+    for (String word : words) {
+      if (!word1.equals(word2)) {
+        if (word.equals(word1)) {
+          w1 = index;
+        } else if (word.equals(word2)) {
+          w2 = index;
         }
-        return distance;
+
+        if (w1 >= 0 && w2 >= 0) {
+          distance = Math.min(distance, Math.abs(w1 - w2));
+        }
+      } else {
+        if (word.equals(word1)) {
+          if (oldIndex < 0) {
+            oldIndex = index;
+          } else {
+            distance = Math.min(distance, Math.abs(oldIndex - index));
+            oldIndex = index;
+          }
+        }
+      }
+      index++;
     }
+    return distance;
+  }
+
+  /** This is a simpler version of the above method which does not handle the case when both words are same.
+   * @param wordsDict Array of words
+   * @param word1 First word
+   * @param word2 Second word
+   * @return Shortest distance between the two words
+   */
+   public int shortestDistanceSimple(String[] wordsDict, String word1, String word2) {
+
+        int w1Index = -1;
+        int w2Index = -1;
+        int minDistance = Integer.MAX_VALUE;
+        for(int i = 0; i < wordsDict.length; ++i) {
+            if(wordsDict[i].equals(word1)) {
+                w1Index = i;
+            }
+            if(wordsDict[i].equals(word2)) {
+                w2Index = i;
+            }
+            if(w2Index >= 0 && w1Index >= 0) {
+                minDistance = Math.min(minDistance, Math.abs(w1Index - w2Index));
+            }
+        }
+        return minDistance;
+    }
+}
 }
