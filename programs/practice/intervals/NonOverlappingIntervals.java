2f248fc6f8ad587d415965474ce822ddae575ee6
RainbowTraveller <milindarvindvaidya@gmail.com>
2025-08-29 20:46:49 -0700

    Formatting

--- programs/practice/intervals/NonOverlappingIntervals.java
+++ programs/practice/intervals/NonOverlappingIntervals.java
@@ -1,7 +1,7 @@
 /*
 Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
 
- 
+
 
 Example 1:
 
@@ -35,40 +35,40 @@ Return ans.
 import java.util.Arrays;
 
 public class NonOverlappingIntervals {
-    public static void main(String[] args) {
-        int count = NonOverlappingIntervals.eraseOverlapIntervals(new int[][]{{1,2},{2,3},{3,4},{1,3}});
-        System.out.println("Count : " + count);
-    }
+  public static void main(String[] args) {
+    int count =
+        NonOverlappingIntervals.eraseOverlapIntervals(new int[][] {{1, 2}, {2, 3}, {3, 4}, {1, 3}});
+    System.out.println("Count : " + count);
+  }
 
-     public static int eraseOverlapIntervals(int[][] intervals) {
-         // Do not sort this way
-        //Arrays.sort(intervals, (i1, i2) -> (i1[0] == i2[0] ? i1[1] - i2[1] : i1[0] - i2[0]));
-        Arrays.sort(intervals, (i1, i2) -> (i1[1] - i2[1]));
-        // OR 
-        //Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
-        int count = 0;
-        //We always start with min value 
-        //Consider this as the end of imaginary previous interval
-        int end = Integer.MIN_VALUE;
-        for(int i = 0; i < intervals.length; ++i) {
-            int x = intervals[i][0];
-            int y = intervals[i][1];
-            //We always compare the start of the next interval and the 
-            // end of the previous interval
-            if(x >= end) {      
-                //if the start of the current interval is 
-                //greater than the end of the previous interval
-                //Clearly there is no overlapping,
-                // so no interval to drop
-                // Mark end to be the end of the current interval
-                end = y;           
-            } else {
-                // If overlapping then drop this interval
-                // hence increament the count
-                count++;
-            }
-        }    
-        return count;
+  public static int eraseOverlapIntervals(int[][] intervals) {
+    // Do not sort this way
+    // Arrays.sort(intervals, (i1, i2) -> (i1[0] == i2[0] ? i1[1] - i2[1] : i1[0] - i2[0]));
+    Arrays.sort(intervals, (i1, i2) -> (i1[1] - i2[1]));
+    // OR
+    // Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+    int count = 0;
+    // We always start with min value
+    // Consider this as the end of imaginary previous interval
+    int end = Integer.MIN_VALUE;
+    for (int i = 0; i < intervals.length; ++i) {
+      int x = intervals[i][0];
+      int y = intervals[i][1];
+      // We always compare the start of the next interval and the
+      // end of the previous interval
+      if (x >= end) {
+        // if the start of the current interval is
+        // greater than the end of the previous interval
+        // Clearly there is no overlapping,
+        // so no interval to drop
+        // Mark end to be the end of the current interval
+        end = y;
+      } else {
+        // If overlapping then drop this interval
+        // hence increament the count
+        count++;
+      }
     }
-
+    return count;
+  }
 }
