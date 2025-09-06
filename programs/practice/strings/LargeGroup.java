f54031ab7e435a4e533db802ea8da2c5248d8251
RainbowTraveller <milindarvindvaidya@gmail.com>
2025-09-06 16:00:19 -0700

    Formatting

--- programs/practice/strings/LargeGroup.java
+++ programs/practice/strings/LargeGroup.java
@@ -24,49 +24,49 @@
  * Input: "abcdddeeeeaabbbcd"
  * Output: [[3,5],[6,9],[12,14]]
  */
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LargeGroup {
 
-    public static void main(String [] args) {
-		String input1 = "abbxxxxzzy";
-		System.out.println("Large groups in : " + input1  + " " + largeGroupPositions(input1));
-		String input2 = "abc";
-		System.out.println("Large groups in : " + input2  + " " + largeGroupPositions(input2));
-		String input3 = "abcdddeeeeaabbbcd";
-		System.out.println("Large groups in : " + input3  + " " + largeGroupPositions(input3));
-    }
+  public static void main(String[] args) {
+    String input1 = "abbxxxxzzy";
+    System.out.println("Large groups in : " + input1 + " " + largeGroupPositions(input1));
+    String input2 = "abc";
+    System.out.println("Large groups in : " + input2 + " " + largeGroupPositions(input2));
+    String input3 = "abcdddeeeeaabbbcd";
+    System.out.println("Large groups in : " + input3 + " " + largeGroupPositions(input3));
+  }
 
-	public static List<List<Integer>> largeGroupPositions(String S) {
-        List<List<Integer>> track = new LinkedList<List<Integer>>();
-        if(S != null && S.length() >= 1) {
+  public static List<List<Integer>> largeGroupPositions(String S) {
+    List<List<Integer>> track = new LinkedList<List<Integer>>();
+    if (S != null && S.length() >= 1) {
 
-            int start = 0;
-            int len = 1;
-            char c = S.charAt(0);
-            for(int i = 1; i < S.length(); ++i) {
-                if(c == S.charAt( i )) {
-                    len++;
-                } else {
-                    if(len >= 3) {
-                        List<Integer> curr = new LinkedList<Integer>();
-                        curr.add(start);
-                        curr.add( i - 1);
-                        track.add(curr);
-                    }
-                    start = i;
-                    c = S.charAt( i );
-                    len = 1;
-                }
-            }
-            if(len >= 3) {
-                List<Integer> curr = new LinkedList<Integer>();
-                curr.add(start);
-                curr.add(S.length() - 1);
-                track.add(curr);
-            }
+      int start = 0;
+      int len = 1;
+      char c = S.charAt(0);
+      for (int i = 1; i < S.length(); ++i) {
+        if (c == S.charAt(i)) {
+          len++;
+        } else {
+          if (len >= 3) {
+            List<Integer> curr = new LinkedList<Integer>();
+            curr.add(start);
+            curr.add(i - 1);
+            track.add(curr);
+          }
+          start = i;
+          c = S.charAt(i);
+          len = 1;
         }
-        return track;
+      }
+      if (len >= 3) {
+        List<Integer> curr = new LinkedList<Integer>();
+        curr.add(start);
+        curr.add(S.length() - 1);
+        track.add(curr);
+      }
     }
+    return track;
+  }
 }
